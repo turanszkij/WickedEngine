4abfb902b97f0301f63487733ab31d8980f4cc1d
turanszkij <turanszkij@gmail.com>
2016-05-14 19:26:43 +0200

    update

--- WickedEngine/toneMapPS.hlsl
+++ WickedEngine/toneMapPS.hlsl
@@ -7,8 +7,8 @@ float4 main(VertexToPixelPostProcess PSIn) : SV_TARGET
 	float4 hdr = xTexture.Load(uint3(PSIn.pos.xy,0));
 	float average_luminance = xMaskTex.Load(uint3(0,0,0)).r;
 
-	if (PSIn.pos.x < 100 && PSIn.pos.y < 100)
-		return float4(average_luminance.xxx,1);
+	//if (PSIn.pos.x < 100 && PSIn.pos.y < 100)
+	//	return float4(average_luminance.xxx,1);
 
 	float luminance = dot(hdr.rgb, float3(0.2126, 0.7152, 0.0722));
 	luminance = (luminance * (1 + luminance)) / (1 + luminance); // Reinhard
