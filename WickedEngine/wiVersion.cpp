2b2f95508e6aafc47e103735528c725089d853ee
turanszkij <turanszkij@gmail.com>
2016-05-18 01:29:10 +0200

    Added graphicsdevice event registration support

--- WickedEngine/wiVersion.cpp
+++ WickedEngine/wiVersion.cpp
@@ -7,7 +7,7 @@ namespace wiVersion
 	// minor features, major bug fixes
 	const int minor = 7;
 	// minor bug fixes, alterations, refactors
-	const int revision = 7;
+	const int revision = 8;
 
 
 	long GetVersion()
