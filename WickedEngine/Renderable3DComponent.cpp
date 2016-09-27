beed279aaea07ef984705f01673f79b8a7f92218
turanszkij <turanszkij@gmail.com>
2016-09-27 23:54:04 +0200

    implement tiled lighting part5

--- WickedEngine/Renderable3DComponent.cpp
+++ WickedEngine/Renderable3DComponent.cpp
@@ -178,7 +178,7 @@ void Renderable3DComponent::Compose(){
 
 	RenderColorGradedComposition();
 
-	wiImage::Draw(wiRenderer::ComputeTiledLightCulling(GRAPHICSTHREAD_IMMEDIATE), wiImageEffects(wiRenderer::GetDevice()->GetScreenWidth(), wiRenderer::GetDevice()->GetScreenHeight()));
+	//wiImage::Draw(wiRenderer::ComputeTiledLightCulling(GRAPHICSTHREAD_IMMEDIATE), wiImageEffects(wiRenderer::GetDevice()->GetScreenWidth(), wiRenderer::GetDevice()->GetScreenHeight()));
 
 	Renderable2DComponent::Compose();
 }
