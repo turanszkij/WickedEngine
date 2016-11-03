2e80c29a82110841dcb79f2769b144e8e1da61b5
turanszkij <turanszkij@gmail.com>
2016-11-04 00:06:31 +0100

    lens flare pixel shader refactored

--- WickedEngine/lensFlarePS.hlsl
+++ WickedEngine/lensFlarePS.hlsl
@@ -1,20 +1,20 @@
 #include "globals.hlsli"
 
 struct VertextoPixel{
-	float4 pos				: SV_POSITION;
-	float3 texPos			: TEXCOORD0;
-	nointerpolation uint   sel				: TEXCOORD1;
-	nointerpolation float4 opa				: TEXCOORD2;
+	float4 pos					: SV_POSITION;
+	float3 texPos				: TEXCOORD0;
+	nointerpolation uint   sel	: TEXCOORD1;
+	nointerpolation float4 opa	: TEXCOORD2;
 };
-
-//Texture2D flare[7]:register(t1);
-
 
 float4 main(VertextoPixel PSIn) : SV_TARGET
 {
 	float4 color=0;
+	
+	// todo: texture atlas or array
 	[branch]
-	switch(PSIn.sel){ // sad :(
+	switch(PSIn.sel)
+	{
 		case 0:
 			color = texture_0.SampleLevel(sampler_linear_clamp, PSIn.texPos.xy, 0);
 			break;
@@ -38,7 +38,9 @@ float4 main(VertextoPixel PSIn) : SV_TARGET
 			break;
 		default:break;
 	};
-	color*=1.1-saturate(PSIn.texPos.z);
-	color*=PSIn.opa.x;
+
+	color *= 1.1 - saturate(PSIn.texPos.z);
+	color *= PSIn.opa.x;
+
 	return color;
 }
