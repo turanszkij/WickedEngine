b53e257bdd92594b38940f58a0732143ff6b0975
Turanszki Janos <turanszkij@gmail.com>
2017-10-22 10:00:29 +0100

    updated editor loading screen

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -87,8 +87,8 @@ void EditorLoadingScreen::Load()
 	sprite = wiSprite("../logo/logo_small.png");
 	sprite.anim.opa = 0.02f;
 	sprite.anim.repeatable = true;
-	sprite.effects.pos = XMFLOAT3(wiRenderer::GetDevice()->GetScreenWidth()*0.9f, wiRenderer::GetDevice()->GetScreenHeight()*0.8f, 0);
-	sprite.effects.siz = XMFLOAT2(128,128);
+	sprite.effects.pos = XMFLOAT3(wiRenderer::GetDevice()->GetScreenWidth()*0.5f, wiRenderer::GetDevice()->GetScreenHeight()*0.5f, 0);
+	sprite.effects.siz = XMFLOAT2(128, 128);
 	sprite.effects.pivot = XMFLOAT2(0.5f, 0.5f);
 	sprite.effects.quality = QUALITY_BILINEAR;
 	sprite.effects.blendFlag = BLENDMODE_ALPHA;
@@ -100,7 +100,7 @@ void EditorLoadingScreen::Compose()
 {
 	__super::Compose();
 
-	wiFont("Loading...", wiFontProps(wiRenderer::GetDevice()->GetScreenWidth() / 2, wiRenderer::GetDevice()->GetScreenHeight() / 2, 50,
+	wiFont("Loading...", wiFontProps((int)(wiRenderer::GetDevice()->GetScreenWidth()*0.5f), (int)(wiRenderer::GetDevice()->GetScreenHeight()*0.6f), 36,
 		WIFALIGN_MID, WIFALIGN_MID)).Draw(GRAPHICSTHREAD_IMMEDIATE);
 }
 void EditorLoadingScreen::Unload()
