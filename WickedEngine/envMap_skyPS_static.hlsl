441c8c95eecee79d48328e9d3f8483afb79e3f0f
turanszkij <turanszkij@gmail.com>
2018-01-26 18:24:13 +0000

    fix in sky rendering for envmaps

--- WickedEngine/envMap_skyPS_static.hlsl
+++ WickedEngine/envMap_skyPS_static.hlsl
@@ -4,5 +4,5 @@
 float4 main(PSIn_Sky input) : SV_TARGET
 {
 	float3 normal = normalize(input.nor);
-	return float4(GetStaticSkyColor(normal), 1);
+	return float4(DEGAMMA(GetStaticSkyColor(normal)), 1);
 }
