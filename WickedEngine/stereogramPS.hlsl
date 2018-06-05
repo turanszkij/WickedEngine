61c9bab64f62e9eebeeafd09b9d27b35f8bc34b9
turanszkij <turanszkij@gmail.com>
2018-06-05 11:54:41 +0100

    fixed stereogram shader

--- WickedEngine/stereogramPS.hlsl
+++ WickedEngine/stereogramPS.hlsl
@@ -3,20 +3,18 @@
 static const float pWid = 100;
 static const float pHei = 100;
 
-#define fragCoord PSIn.pos.xy
-
 float4 main(VertexToPixelPostProcess PSIn) : SV_TARGET
 {
 	float maxStep = 32.;
 	float d = 0.;
 
-	float2 uv = fragCoord.xy;
+	float2 uv = PSIn.pos.xy;
 
 	for (int count = 0; count < 100; count++) {
 		if (uv.x < pWid)
 			break;
 
-		float d = 1.0 - texture_lineardepth.SampleLevel(sampler_linear_clamp, uv / g_xWorld_ScreenWidthHeight.xy, 0).r / g_xFrame_MainCamera_ZFarP;
+		float d = 1.0 - saturate(texture_lineardepth.SampleLevel(sampler_linear_clamp, uv / g_xWorld_ScreenWidthHeight.xy, 0).r * 100);
 
 		uv.x -= pWid - (d * maxStep);
 	}
