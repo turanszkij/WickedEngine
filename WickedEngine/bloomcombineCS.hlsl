fbb990f207771df93a50ad51f9ecd8deb95562fb
turanszkij <turanszkij@gmail.com>
2020-03-23 00:26:41 +0000

    bloom combine fix

--- WickedEngine/bloomcombineCS.hlsl
+++ WickedEngine/bloomcombineCS.hlsl
@@ -2,7 +2,7 @@
 #include "ShaderInterop_Postprocess.h"
 
 TEXTURE2D(input, float4, TEXSLOT_ONDEMAND0);
-TEXTURE2D(bloom, float4, TEXSLOT_ONDEMAND1);
+TEXTURE2D(texture_bloom, float3, TEXSLOT_ONDEMAND1);
 
 RWTEXTURE2D(output, float4, 0);
 
@@ -11,10 +11,10 @@ void main(uint3 DTid : SV_DispatchThreadID)
 {
 	const float2 uv = (DTid.xy + 0.5f) * xPPResolution_rcp;
 
-	float4 color = input[DTid.xy];
-	color.rgb += bloom.SampleLevel(sampler_linear_clamp, uv, 1.5f).rgb;
-	color.rgb += bloom.SampleLevel(sampler_linear_clamp, uv, 3.5f).rgb;
-	color.rgb += bloom.SampleLevel(sampler_linear_clamp, uv, 4.5f).rgb;
+	float3 bloom = texture_bloom.SampleLevel(sampler_linear_clamp, uv, 1.5f);
+	bloom += texture_bloom.SampleLevel(sampler_linear_clamp, uv, 3.5f);
+	bloom += texture_bloom.SampleLevel(sampler_linear_clamp, uv, 4.5f);
+	bloom /= 3.0f;
 
-	output[DTid.xy] = color;
+	output[DTid.xy] = input[DTid.xy] + float4(bloom, 0);
 }
