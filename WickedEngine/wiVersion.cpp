cafc4ba4dd352551f792a37892cd9dddd39a8fb4
Turanszki Janos <turanszkij@gmail.com>
2020-06-17 19:47:15 +0100

    improved armature bounds creation

--- WickedEngine/wiVersion.cpp
+++ WickedEngine/wiVersion.cpp
@@ -9,7 +9,7 @@ namespace wiVersion
 	// minor features, major updates
 	const int minor = 42;
 	// minor bug fixes, alterations, refactors, updates
-	const int revision = 10;
+	const int revision = 11;
 
 	const std::string version_string = std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(revision);
 
