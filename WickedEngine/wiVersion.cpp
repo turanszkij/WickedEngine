c12306765d6a1e26327aa0b7b548ed5176cd5c87
Turanszki Janos <turanszkij@gmail.com>
2020-08-23 19:04:21 +0100

    subsurface scattering improvement

--- WickedEngine/wiVersion.cpp
+++ WickedEngine/wiVersion.cpp
@@ -9,7 +9,7 @@ namespace wiVersion
 	// minor features, major updates, breaking API changes
 	const int minor = 47;
 	// minor bug fixes, alterations, refactors, updates
-	const int revision = 20;
+	const int revision = 21;
 
 	const std::string version_string = std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(revision);
 
