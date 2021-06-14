b6a1e0796af4eb2dc9c715899011ac5d1bfd3f7a
Turanszki Janos <turanszkij@gmail.com>
2021-06-14 15:21:31 +0200

    raw input fix: allocations must use 8-byte alignment

--- WickedEngine/wiAllocators.h
+++ WickedEngine/wiAllocators.h
@@ -5,24 +5,43 @@
 
 namespace wiAllocators
 {
+	inline size_t Align(size_t uLocation, size_t uAlign)
+	{
+		if ((0 == uAlign) || (uAlign & (uAlign - 1)))
+		{
+			assert(0);
+		}
+
+		return ((uLocation + (uAlign - 1)) & ~(uAlign - 1));
+	}
 
 	class LinearAllocator
 	{
 	public:
+
+		~LinearAllocator()
+		{
+			_mm_free(buffer);
+		}
 
 		inline size_t get_capacity() const
 		{
 			return capacity;
 		}
 
-		inline void reserve(size_t newCapacity)
+		inline void reserve(size_t newCapacity, size_t align = 1)
 		{
+			alignment = align;
+			newCapacity = Align(newCapacity, alignment);
 			capacity = newCapacity;
-			buffer = std::make_unique<uint8_t[]>(capacity);
+
+			_mm_free(buffer);
+			buffer = (uint8_t*)_mm_malloc(capacity, alignment);
 		}
 
 		inline uint8_t* allocate(size_t size)
 		{
+			size = Align(size, alignment);
 			if (offset + size <= capacity)
 			{
 				uint8_t* ret = &buffer[offset];
@@ -34,6 +53,7 @@ namespace wiAllocators
 
 		inline void free(size_t size)
 		{
+			size = Align(size, alignment);
 			assert(offset >= size);
 			offset -= size;
 		}
@@ -49,9 +69,10 @@ namespace wiAllocators
 		}
 
 	private:
-		std::unique_ptr<uint8_t[]> buffer;
+		uint8_t* buffer = nullptr;
 		size_t capacity = 0;
 		size_t offset = 0;
+		size_t alignment = 1;
 	};
 
 }
