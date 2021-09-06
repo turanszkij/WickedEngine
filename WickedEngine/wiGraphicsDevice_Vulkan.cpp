a03aff0a9d6632694257374b4d9d1c5d3c20c22d
Turanszki Janos <turanszkij@gmail.com>
2021-09-06 10:12:12 +0200

    fix: incorrect initialization of non-POD data

--- WickedEngine/wiGraphicsDevice_Vulkan.cpp
+++ WickedEngine/wiGraphicsDevice_Vulkan.cpp
@@ -1229,13 +1229,34 @@ using namespace Vulkan_Internal;
 			assert(res == VK_SUCCESS);
 		}
 
-		memset(CBV, 0, sizeof(CBV));
-		memset(CBV_offset, 0, sizeof(CBV_offset));
-		memset(SRV, 0, sizeof(SRV));
-		memset(SRV_index, -1, sizeof(SRV_index));
-		memset(UAV, 0, sizeof(UAV));
-		memset(UAV_index, -1, sizeof(UAV_index));
-		memset(SAM, 0, sizeof(SAM));
+		for (int i = 0; i < arraysize(CBV); ++i)
+		{
+			CBV[i] = {};
+		}
+		for (int i = 0; i < arraysize(CBV_offset); ++i)
+		{
+			CBV_offset[i] = {};
+		}
+		for (int i = 0; i < arraysize(SRV); ++i)
+		{
+			SRV[i] = {};
+		}
+		for (int i = 0; i < arraysize(SRV_index); ++i)
+		{
+			SRV_index[i] = {};
+		}
+		for (int i = 0; i < arraysize(UAV); ++i)
+		{
+			UAV[i] = {};
+		}
+		for (int i = 0; i < arraysize(UAV_index); ++i)
+		{
+			UAV_index[i] = {};
+		}
+		for (int i = 0; i < arraysize(SAM); ++i)
+		{
+			SAM[i] = {};
+		}
 	}
 	void GraphicsDevice_Vulkan::FrameResources::DescriptorBinder::flush(bool graphics, CommandList cmd)
 	{
