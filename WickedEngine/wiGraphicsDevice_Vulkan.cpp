173ca329c2fbb16a1256f8986ea99dd33dbbbcf8
Amer Koleci <amerkoleci@gmail.com>
2021-09-10 09:00:22 +0200

    Vulkan: Add new VK_CHECK to check and log error during calls.

--- WickedEngine/wiGraphicsDevice_Vulkan.cpp
+++ WickedEngine/wiGraphicsDevice_Vulkan.cpp
@@ -38,6 +38,42 @@ namespace wiGraphics
 
 namespace Vulkan_Internal
 {
+	const char* ToString(VkResult result)
+	{
+		switch (result)
+		{
+#define STR(r)   \
+	case VK_##r: \
+		return #r
+			STR(NOT_READY);
+			STR(TIMEOUT);
+			STR(EVENT_SET);
+			STR(EVENT_RESET);
+			STR(INCOMPLETE);
+			STR(ERROR_OUT_OF_HOST_MEMORY);
+			STR(ERROR_OUT_OF_DEVICE_MEMORY);
+			STR(ERROR_INITIALIZATION_FAILED);
+			STR(ERROR_DEVICE_LOST);
+			STR(ERROR_MEMORY_MAP_FAILED);
+			STR(ERROR_LAYER_NOT_PRESENT);
+			STR(ERROR_EXTENSION_NOT_PRESENT);
+			STR(ERROR_FEATURE_NOT_PRESENT);
+			STR(ERROR_INCOMPATIBLE_DRIVER);
+			STR(ERROR_TOO_MANY_OBJECTS);
+			STR(ERROR_FORMAT_NOT_SUPPORTED);
+			STR(ERROR_SURFACE_LOST_KHR);
+			STR(ERROR_NATIVE_WINDOW_IN_USE_KHR);
+			STR(SUBOPTIMAL_KHR);
+			STR(ERROR_OUT_OF_DATE_KHR);
+			STR(ERROR_INCOMPATIBLE_DISPLAY_KHR);
+			STR(ERROR_VALIDATION_FAILED_EXT);
+			STR(ERROR_INVALID_SHADER_NV);
+#undef STR
+			default:
+				return "UNKNOWN_ERROR";
+		}
+	}
+
 	// Converters:
 	constexpr VkFormat _ConvertFormat(FORMAT value)
 	{
@@ -967,8 +1003,34 @@ namespace Vulkan_Internal
 	{
 		return static_cast<SwapChain_Vulkan*>(param->internal_state.get());
 	}
+
+	inline void Log(const char* format, ...)
+	{
+		char msg[1024];
+		va_list args;
+		va_start(args, format);
+		vsnprintf(msg, sizeof(msg), format, args);
+		va_end(args);
+
+		std::clog << msg;
+#ifdef _WIN32
+		OutputDebugStringA(msg);
+#endif
+	}
 }
 using namespace Vulkan_Internal;
+
+/// Helper macro to test the result of Vulkan calls which can return an error.
+#define VK_CHECK(x)                                                 \
+	do                                                              \
+	{                                                               \
+		VkResult err = x;                                           \
+		if (err)                                                    \
+		{                                                           \
+			Log("Detected Vulkan error: %s", Vulkan_Internal::ToString(err)); \
+			abort();                                                \
+		}                                                           \
+	} while (0)
 
 	// Allocators:
 
@@ -1877,7 +1939,7 @@ using namespace Vulkan_Internal;
 		VkResult res;
 
 		res = volkInitialize();
-		assert(res == VK_SUCCESS);
+		VK_CHECK(res);
 
 		// Fill out application info:
 		VkApplicationInfo appInfo = {};
@@ -1890,18 +1952,14 @@ using namespace Vulkan_Internal;
 
 		// Enumerate available layers and extensions:
 		uint32_t instanceLayerCount;
-		res = vkEnumerateInstanceLayerProperties(&instanceLayerCount, nullptr);
-		assert(res == VK_SUCCESS);
+		VK_CHECK(vkEnumerateInstanceLayerProperties(&instanceLayerCount, nullptr));
 		std::vector<VkLayerProperties> availableInstanceLayers(instanceLayerCount);
-		res = vkEnumerateInstanceLayerProperties(&instanceLayerCount, availableInstanceLayers.data());
-		assert(res == VK_SUCCESS);
+		VK_CHECK(vkEnumerateInstanceLayerProperties(&instanceLayerCount, availableInstanceLayers.data()));
 
 		uint32_t extensionCount = 0;
-		res = vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-		assert(res == VK_SUCCESS);
+		VK_CHECK(vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr));
 		std::vector<VkExtensionProperties> availableInstanceExtensions(extensionCount);
-		res = vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, availableInstanceExtensions.data());
-		assert(res == VK_SUCCESS);
+		VK_CHECK(vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, availableInstanceExtensions.data()));
 
 		std::vector<const char*> instanceLayers;
 		std::vector<const char*> instanceExtensions;
