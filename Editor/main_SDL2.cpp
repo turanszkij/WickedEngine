19006f5772053d10ed2b85a09cd2df7711a09c69
sashikknox <savegame@users.noreply.github.com>
2021-09-22 12:48:28 +0300

    - add Editor icon for SDL window (builtin) (#319)

--- Editor/main_SDL2.cpp
+++ Editor/main_SDL2.cpp
@@ -9,6 +9,8 @@
 #include <SDL2/SDL.h>
 #include "sdl2.h"
 
+#include "Assets/Icon.c"
+
 using namespace std;
 
 //Convert SDL scancode to character input
@@ -137,6 +139,31 @@ int sdl_loop(Editor &editor)
     return 0;
 }
 
+void set_window_icon(SDL_Window *window) {
+    // these masks are needed to tell SDL_CreateRGBSurface(From)
+    // to assume the data it gets is byte-wise RGB(A) data
+    Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (gimp_image.bytes_per_pixel == 3) ? 8 : 0;
+    rmask = 0xff000000 >> shift;
+    gmask = 0x00ff0000 >> shift;
+    bmask = 0x0000ff00 >> shift;
+    amask = 0x000000ff >> shift;
+#else // little endian, like x86
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = (gimp_image.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom((void*)gimp_image.pixel_data, gimp_image.width,
+        gimp_image.height, gimp_image.bytes_per_pixel*8, gimp_image.bytes_per_pixel*gimp_image.width,
+        rmask, gmask, bmask, amask);
+
+    SDL_SetWindowIcon(window, icon);
+ 
+    SDL_FreeSurface(icon);
+}
+
 int main(int argc, char *argv[])
 {
     Editor editor;
@@ -173,6 +200,8 @@ int main(int argc, char *argv[])
     if (!window) {
         throw sdl2::SDLError("Error creating window");
     }
+
+    set_window_icon( window.get());
 
     if(fullscreen) SDL_SetWindowFullscreen(window.get(), SDL_TRUE);
 
