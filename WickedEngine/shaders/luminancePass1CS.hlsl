7b2556d5a2dfa42fddcfd732b43d55dffc408daf
Turánszki János <turanszkij@users.noreply.github.com>
2021-09-28 00:03:59 +0200

    eye adaption improvements

--- WickedEngine/shaders/luminancePass1CS.hlsl
+++ WickedEngine/shaders/luminancePass1CS.hlsl
@@ -8,9 +8,17 @@ TEXTURE2D(input, float3, TEXSLOT_ONDEMAND0);
 
 RWRAWBUFFER(output_histogram, 0);
 
+// groupshared memory is used to perform the high atomic contentions
+//	the atomic contention into global memory will be lower in the general case
+//	So even though this increases number of atomic ops, performance will be better
+groupshared uint shared_bins[LUMINANCE_NUM_HISTOGRAM_BINS];
+
 [numthreads(LUMINANCE_BLOCKSIZE, LUMINANCE_BLOCKSIZE, 1)]
 void main(uint3 DTid : SV_DispatchThreadID, uint groupIndex : SV_GroupIndex)
 {
+	shared_bins[groupIndex] = 0;
+	GroupMemoryBarrierWithGroupSync();
+
 	if (DTid.x < postprocess.resolution.x && DTid.y < postprocess.resolution.y)
 	{
 		const float2 uv = (DTid.xy + 0.5) * postprocess.resolution_rcp;
@@ -18,11 +26,16 @@ void main(uint3 DTid : SV_DispatchThreadID, uint groupIndex : SV_GroupIndex)
 		float luminance = dot(color, float3(0.2127f, 0.7152f, 0.0722f));
 
 		uint binIndex = 0;
-		if (luminance > 0.01)
+		if (luminance > 0.001)
 		{
 			float logLuminance = saturate((log2(luminance) - luminance_log_min) * luminance_log_range_rcp);
 			binIndex = (uint)(logLuminance * (LUMINANCE_NUM_HISTOGRAM_BINS - 2) + 1);
 		}
-		output_histogram.InterlockedAdd(LUMINANCE_BUFFER_OFFSET_HISTOGRAM + binIndex * 4, 1);
+
+		InterlockedAdd(shared_bins[binIndex], 1);
 	}
+
+	GroupMemoryBarrierWithGroupSync();
+	output_histogram.InterlockedAdd(LUMINANCE_BUFFER_OFFSET_HISTOGRAM + groupIndex * 4, shared_bins[groupIndex]);
+
 }
