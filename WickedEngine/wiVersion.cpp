e3611fba20df88357ef509b15adee50209680377
Turánszki János <turanszkij@users.noreply.github.com>
2021-10-10 19:10:45 +0200

    font renderer: use premultiplied alpha blending

--- WickedEngine/wiVersion.cpp
+++ WickedEngine/wiVersion.cpp
@@ -9,7 +9,7 @@ namespace wiVersion
 	// minor features, major updates, breaking compatibility changes
 	const int minor = 58;
 	// minor bug fixes, alterations, refactors, updates
-	const int revision = 5;
+	const int revision = 6;
 
 	const std::string version_string = std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(revision);
 
