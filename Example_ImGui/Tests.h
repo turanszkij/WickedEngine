3bc486bd1dd139dc382ddfa2e84bd275e8b47d45
Amer Koleci <amerkoleci@gmail.com>
2021-11-03 14:05:53 +0100

    Examples: Initial ImGui 1.85 integration example.

---
+++ Example_ImGui/Tests.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "WickedEngine.h"
+
+
+class TestsRenderer : public RenderPath3D
+{
+	wiLabel label;
+	wiComboBox testSelector;
+	wiECS::Entity ik_entity = wiECS::INVALID_ENTITY;
+public:
+	void Load() override;
+	void Update(float dt) override;
+	void ResizeLayout() override;
+
+	void RunJobSystemTest();
+	void RunFontTest();
+	void RunSpriteTest();
+	void RunNetworkTest();
+};
+
+class Tests : public MainComponent
+{
+	TestsRenderer renderer;
+public:
+	~Tests() override;
+	void Initialize() override;
+};
+
---
+++ Example_ImGui/stdafx.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "WickedEngine.h"
+#include "Tests.h"
