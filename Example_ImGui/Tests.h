014c498aae17a2dbc294701519bfa047769c8372
Amer Koleci <amerkoleci@gmail.com>
2021-11-03 16:38:39 +0100

    ImGUI integration: Cleanup example.

--- Example_ImGui/Tests.h
+++ Example_ImGui/Tests.h
@@ -5,18 +5,12 @@
 class TestsRenderer : public RenderPath3D
 {
 	wiLabel label;
-	wiComboBox testSelector;
 	wiECS::Entity ik_entity = wiECS::INVALID_ENTITY;
 public:
 	void Load() override;
 	void Update(float dt) override;
 	void ResizeLayout() override;
 	void Render() const override;
-
-	void RunJobSystemTest();
-	void RunFontTest();
-	void RunSpriteTest();
-	void RunNetworkTest();
 };
 
 class Tests : public MainComponent
