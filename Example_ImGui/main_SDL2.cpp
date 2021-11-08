1c81281ca16118c74df067e23232c48447548b52
Matteo De Carlo <matteo.dek@gmail.com>
2021-11-08 19:32:21 +0100

    Resizable windows on SDL2 (linux)
    Fixes #349

--- Example_ImGui/main_SDL2.cpp
+++ Example_ImGui/main_SDL2.cpp
@@ -16,25 +16,26 @@ int sdl_loop(Example_ImGui &tests)
         SDL_PumpEvents();
         tests.Run();
 
-//        int ret = SDL_PeepEvents(&event, 1, SDL_GETEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT);
         while (SDL_PollEvent(&event)) {
-
-            if (event.type == SDL_WINDOWEVENT) {
+            switch (event.type) {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            case SDL_WINDOWEVENT:
                 switch (event.window.event) {
-                    case SDL_WINDOWEVENT_CLOSE:   // exit game
-                        //tests.Quit();
-                        quit = true;
-
-                    default:
-                        break;
+                case SDL_WINDOWEVENT_CLOSE:   // exit game
+                    quit = true;
+                    break;
+                case SDL_WINDOWEVENT_RESIZED:
+                    // Tells the engine to reload window configuration (size and dpi)
+                    tests.SetWindow(tests.window);
+                    break;
+                default:
+                    break;
                 }
+            default:
+                break;
             }
-//            else if (event.type == SDL_KEYDOWN) {
-//                switch (event.key.keysym.scancode) {
-//                    case SDL_SCANCODE_SPACE:
-//                        tests.SetSelected(tests.GetSelected()+1);
-//                }
-//            }
 
             ImGui_ImplSDL2_ProcessEvent(&event);
         }
@@ -60,7 +61,7 @@ int main(int argc, char *argv[])
             "Wicked Engine Tests",
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             1280, 800,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
     if (!window) {
         throw sdl2::SDLError("Error creating window");
     }
