6f5e091fb818c13f2f4fa77d2d61d5dfc35faf89
Turánszki János <turanszkij@users.noreply.github.com>
2021-11-25 19:43:18 +0100

    allocator fix

--- WickedEngine/wiRawInput.cpp
+++ WickedEngine/wiRawInput.cpp
@@ -14,7 +14,72 @@
 
 namespace wiRawInput
 {
-	wiAllocators::AlignedLinearAllocator allocator;
+
+	constexpr size_t Align(size_t value, size_t alignment)
+	{
+		return ((value + alignment - 1) / alignment) * alignment;
+	}
+
+	class AlignedLinearAllocator
+	{
+	public:
+		~AlignedLinearAllocator()
+		{
+			_aligned_free(buffer);
+		}
+
+		constexpr size_t get_capacity() const
+		{
+			return capacity;
+		}
+
+		inline void reserve(size_t newCapacity, size_t align)
+		{
+			alignment = align;
+			newCapacity = Align(newCapacity, alignment);
+			capacity = newCapacity;
+
+			_aligned_free(buffer);
+			buffer = (uint8_t*)_aligned_malloc(capacity, alignment);
+		}
+
+		constexpr uint8_t* allocate(size_t size)
+		{
+			size = Align(size, alignment);
+			if (offset + size <= capacity)
+			{
+				uint8_t* ret = &buffer[offset];
+				offset += size;
+				return ret;
+			}
+			return nullptr;
+		}
+
+		constexpr void free(size_t size)
+		{
+			size = Align(size, alignment);
+			assert(offset >= size);
+			offset -= size;
+		}
+
+		constexpr void reset()
+		{
+			offset = 0;
+		}
+
+		constexpr uint8_t* top()
+		{
+			return &buffer[offset];
+		}
+
+	private:
+		uint8_t* buffer = nullptr;
+		size_t capacity = 0;
+		size_t offset = 0;
+		size_t alignment = 1;
+	};
+
+	AlignedLinearAllocator allocator;
 	std::vector<uint8_t*> input_messages;
 
 	wiInput::KeyboardState keyboard;
