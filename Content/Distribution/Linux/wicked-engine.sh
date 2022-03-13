b03ccf2d39f0c052dbe3c2c107a1b06ed8022780
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-03-13 15:08:55 +0100

    Wrote Linux package files

---
+++ Content/Distribution/Linux/wicked-engine.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+# This is Wicked Engine launch script for package maintainers. 
+# Wicked Engine binaries must be launched from their root directory.
+# That's why a launcher script is necessary. Sorry for the inconvenience!
+
+cd $WICKED && ./WickedEngineEditor # Replace the $WICKED variable with the binary path.
