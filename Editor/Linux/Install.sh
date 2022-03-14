09f035f7f6d312360383ca426d0fa42edb2ea1a8
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-03-14 21:27:03 +0100

    More installer changes

--- Editor/Linux/Install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-sudo rm /usr/bin/wicked-engine
-sudo cp wicked-engine.sh /usr/bin/wicked-engine
-
-sudo chmod +x /usr/bin/wicked-engine
-
-sudo rm /usr/share/applications/wicked-engine.desktop
-sudo cp wicked-engine.desktop /usr/share/applications/wicked-engine.desktop
