e230bab3be862bc101b39c6c61edb7909d412ace
Turánszki János <turanszkij@users.noreply.github.com>
2022-04-07 11:09:09 +0200

    mesh window lod count display

--- Editor/MeshWindow.cpp
+++ Editor/MeshWindow.cpp
@@ -1404,7 +1404,7 @@ void MeshWindow::SetEntity(Entity entity, int subset)
 		ss += "Mesh name: " + name.name + "\n";
 		ss += "Vertex count: " + std::to_string(mesh->vertex_positions.size()) + "\n";
 		ss += "Index count: " + std::to_string(mesh->indices.size()) + "\n";
-		ss += "Subset count: " + std::to_string(mesh->subsets.size()) + "\n";
+		ss += "Subset count: " + std::to_string(mesh->subsets.size()) + " (" + std::to_string(mesh->GetLODCount()) + " LODs)\n";
 		ss += "GPU memory: " + std::to_string((mesh->generalBuffer.GetDesc().size + mesh->streamoutBuffer.GetDesc().size) / 1024.0f / 1024.0f) + " MB\n";
 		ss += "\nVertex buffers: ";
 		if (mesh->vb_pos_nor_wind.IsValid()) ss += "position; ";
