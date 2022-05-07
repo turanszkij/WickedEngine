4a3256a4ee6782a9e7fe197d48919efce560a8fa
Turánszki János <turanszkij@users.noreply.github.com>
2022-05-07 13:20:51 +0200

    refactor

--- WickedEngine/shaders/visibility_shadeCS.hlsl
+++ WickedEngine/shaders/visibility_shadeCS.hlsl
@@ -18,6 +18,11 @@ void main(uint DTid : SV_DispatchThreadID, uint groupIndex : SV_GroupIndex, uint
 		return;
 
 	uint2 pixel = unpack_pixel(binned_pixels[bin.offset + DTid.x]);
+	uint primitiveID = texture_primitiveID[pixel];
+	if (!any(primitiveID))
+	{
+		return;
+	}
 
 	const float2 uv = ((float2)pixel + 0.5) * GetCamera().internal_resolution_rcp;
 	const float2 clipspace = uv_to_clipspace(uv);
@@ -26,70 +31,64 @@ void main(uint DTid : SV_DispatchThreadID, uint groupIndex : SV_GroupIndex, uint
 	float3 rayDirection_quad_x = CreateCameraRay(uv_to_clipspace(((float2)pixel + float2(1, 0) + 0.5) * GetCamera().internal_resolution_rcp)).Direction;
 	float3 rayDirection_quad_y = CreateCameraRay(uv_to_clipspace(((float2)pixel + float2(0, 1) + 0.5) * GetCamera().internal_resolution_rcp)).Direction;
 
-	uint primitiveID = texture_primitiveID[pixel];
-	if (any(primitiveID))
+	PrimitiveID prim;
+	prim.unpack(primitiveID);
+
+	Surface surface;
+	surface.init();
+
+	[branch]
+	if (!surface.load(prim, ray.Origin, ray.Direction, rayDirection_quad_x, rayDirection_quad_y))
 	{
-		PrimitiveID prim;
-		prim.unpack(primitiveID);
+		return;
+	}
 
-		Surface surface;
-		surface.init();
+	surface.pixel = pixel.xy;
+	surface.screenUV = uv;
 
-		[branch]
-		if (surface.load(prim, ray.Origin, ray.Direction, rayDirection_quad_x, rayDirection_quad_y))
-		{
-			surface.pixel = pixel.xy;
-			surface.screenUV = uv;
+	if ((surface.flags & SURFACE_FLAG_GI_APPLIED) == 0)
+	{
+		float3 ambient = GetAmbient(surface.N);
+		surface.gi = lerp(ambient, ambient * surface.sss.rgb, saturate(surface.sss.a));
+	}
 
-			if ((surface.flags & SURFACE_FLAG_GI_APPLIED) == 0)
-			{
-				float3 ambient = GetAmbient(surface.N);
-				surface.gi = lerp(ambient, ambient * surface.sss.rgb, saturate(surface.sss.a));
-			}
+	Lighting lighting;
+	lighting.create(0, 0, surface.gi, 0);
 
-			Lighting lighting;
-			lighting.create(0, 0, surface.gi, 0);
-
-			ApplyEmissive(surface, lighting);
+	ApplyEmissive(surface, lighting);
 
 
 #ifdef PLANARREFLECTION
-			lighting.indirect.specular += PlanarReflection(surface, surface.N.xz) * surface.F;
+	lighting.indirect.specular += PlanarReflection(surface, surface.N.xz) * surface.F;
 #endif
 
-			TiledLighting(surface, lighting);
+	TiledLighting(surface, lighting);
 
-			[branch]
-			if (GetCamera().texture_ssr_index >= 0)
-			{
-				float4 ssr = bindless_textures[GetCamera().texture_ssr_index].SampleLevel(sampler_linear_clamp, surface.screenUV, 0);
-				lighting.indirect.specular = lerp(lighting.indirect.specular, ssr.rgb * surface.F, ssr.a);
-			}
-			[branch]
-			if (GetCamera().texture_ao_index >= 0)
-			{
-				surface.occlusion *= bindless_textures_float[GetCamera().texture_ao_index].SampleLevel(sampler_linear_clamp, surface.screenUV, 0).r;
-			}
+	[branch]
+	if (GetCamera().texture_ssr_index >= 0)
+	{
+		float4 ssr = bindless_textures[GetCamera().texture_ssr_index].SampleLevel(sampler_linear_clamp, surface.screenUV, 0);
+		lighting.indirect.specular = lerp(lighting.indirect.specular, ssr.rgb * surface.F, ssr.a);
+	}
+	[branch]
+	if (GetCamera().texture_ao_index >= 0)
+	{
+		surface.occlusion *= bindless_textures_float[GetCamera().texture_ao_index].SampleLevel(sampler_linear_clamp, surface.screenUV, 0).r;
+	}
 
 #ifdef UNLIT
-			lighting.direct.diffuse = 1;
-			lighting.indirect.diffuse = 0;
-			lighting.direct.specular = 0;
-			lighting.indirect.specular = 0;
+	lighting.direct.diffuse = 1;
+	lighting.indirect.diffuse = 0;
+	lighting.direct.specular = 0;
+	lighting.indirect.specular = 0;
 #endif // UNLIT
 
-			float4 color = 0;
+	float4 color = 0;
 
-			ApplyLighting(surface, lighting, color);
+	ApplyLighting(surface, lighting, color);
 
-			ApplyFog(surface.hit_depth, GetCamera().position, surface.V, color);
+	ApplyFog(surface.hit_depth, GetCamera().position, surface.V, color);
 
-			output[pixel] = float4(color.rgb, 1);
-			//output[pixel] = float4(surface.albedo, 1);
-		}
-	}
-	else
-	{
-		// sky
-	}
+	output[pixel] = float4(color.rgb, 1);
+
 }
