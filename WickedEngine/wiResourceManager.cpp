581dd801d42acfc4153d861d4dc12675817f9f9e
Igor Alexey <igor-alexey@hotmail.com>
2022-05-08 11:53:52 -0300

    QOI decoding Implementation
    So, since the engine is focused on contemporary rendering techniques and performance, I thought why not integrate QOI?
    www.qoiformat.org/
    It seems to be 3x to 4x quicker at decoding picture files compared to stb_image; this might be a big benefit for extremely high resolution imagery (up to 20,000*20,000 max resolution)

--- WickedEngine/wiResourceManager.cpp
+++ WickedEngine/wiResourceManager.cpp
@@ -5,6 +5,8 @@
 #include "wiUnorderedMap.h"
 
 #include "Utility/stb_image.h"
+#define QOI_IMPLEMENTATION // TODO: Move this to wherever turanszkij did the STB_IMAGE_IMPLEMENTATION, yes im looking at you o-o
+#include "Utility/qoi.h"
 #include "Utility/tinyddsloader.h"
 #include "Utility/basis_universal/transcoder/basisu_transcoder.h"
 extern basist::etc1_global_selector_codebook g_basis_global_codebook;
@@ -106,6 +108,7 @@ namespace wi
 			std::make_pair("BMP", DataType::IMAGE),
 			std::make_pair("DDS", DataType::IMAGE),
 			std::make_pair("TGA", DataType::IMAGE),
+			std::make_pair("QOI", DataType::IMAGE),
 			std::make_pair("WAV", DataType::SOUND),
 			std::make_pair("OGG", DataType::SOUND),
 		};
@@ -512,6 +515,90 @@ namespace wi
 					}
 					else assert(0); // failed to load DDS
 
+				}
+				else if (!ext.compare("QOI"))
+				{
+					// Load QOI image
+					qoi_desc desc;
+					const int channelCount = 4;
+					void* rgba_pixels = qoi_decode(filedata, (int)filesize, &desc, channelCount);
+
+					if (rgba_pixels != nullptr)
+					{
+						TextureDesc tDesc;
+						tDesc.width = desc.width;
+						tDesc.height = desc.height;
+						tDesc.layout = ResourceState::SHADER_RESOURCE;
+
+						if (has_flag(flags, Flags::IMPORT_COLORGRADINGLUT))
+						{
+							if (tDesc.type != TextureDesc::Type::TEXTURE_2D ||
+								tDesc.width != 256 ||
+								tDesc.height != 16)
+							{
+								wi::helper::messageBox("The Dimensions must be 256 x 16 for color grading LUT!", "Error");
+							}
+							else
+							{
+								uint32_t data[16 * 16 * 16];
+								int pixel = 0;
+								for (int z = 0; z < 16; ++z)
+								{
+									for (int y = 0; y < 16; ++y)
+									{
+										for (int x = 0; x < 16; ++x)
+										{
+											int coord = x + y * 256 + z * 16;
+											data[pixel++] = ((uint32_t*)rgba_pixels)[coord];
+										}
+									}
+								}
+
+								tDesc.type = TextureDesc::Type::TEXTURE_3D;
+								tDesc.width = 16;
+								tDesc.height = 16;
+								tDesc.depth = 16;
+								tDesc.format = Format::R8G8B8A8_UNORM;
+								tDesc.bind_flags = BindFlag::SHADER_RESOURCE;
+								SubresourceData InitData;
+								InitData.data_ptr = data;
+								InitData.row_pitch = 16 * sizeof(uint32_t);
+								InitData.slice_pitch = 16 * InitData.row_pitch;
+								success = device->CreateTexture(&tDesc, &InitData, &resource->texture);
+								device->SetName(&resource->texture, name.c_str());
+							}
+						}
+						else
+						{
+							tDesc.bind_flags = BindFlag::SHADER_RESOURCE | BindFlag::UNORDERED_ACCESS;
+							tDesc.format = Format::R8G8B8A8_UNORM;
+							tDesc.mip_levels = (uint32_t)log2(std::max(tDesc.width, tDesc.width)) + 1;
+							tDesc.usage = Usage::DEFAULT;
+							tDesc.layout = ResourceState::SHADER_RESOURCE;
+
+							uint32_t mipwidth = tDesc.width;
+							wi::vector<SubresourceData> InitData(tDesc.mip_levels);
+							for (uint32_t mip = 0; mip < tDesc.mip_levels; ++mip)
+							{
+								InitData[mip].data_ptr = rgba_pixels; // attention! we don't fill the mips here correctly, just always point to the mip0 data by default. Mip levels will be created using compute shader when needed!
+								InitData[mip].row_pitch = static_cast<uint32_t>(mipwidth * channelCount);
+								mipwidth = std::max(1u, mipwidth / 2);
+							}
+
+							success = device->CreateTexture(&tDesc, InitData.data(), &resource->texture);
+							device->SetName(&resource->texture, name.c_str());
+
+							for (uint32_t i = 0; i < resource->texture.desc.mip_levels; ++i)
+							{
+								int subresource_index;
+								subresource_index = device->CreateSubresource(&resource->texture, SubresourceType::SRV, 0, 1, i, 1);
+								assert(subresource_index == i);
+								subresource_index = device->CreateSubresource(&resource->texture, SubresourceType::UAV, 0, 1, i, 1);
+								assert(subresource_index == i);
+							}
+						}
+						free(rgba_pixels);
+					}
 				}
 				else
 				{
