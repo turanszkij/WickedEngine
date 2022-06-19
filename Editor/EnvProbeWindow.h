4673bcdc9634cdf0528e6398213eff179a39c8d7
Turánszki János <turanszkij@users.noreply.github.com>
2022-06-19 11:17:35 +0200

    envprobe window update

--- Editor/EnvProbeWindow.h
+++ Editor/EnvProbeWindow.h
@@ -11,6 +11,7 @@ public:
 	wi::ecs::Entity entity;
 	void SetEntity(wi::ecs::Entity entity);
 
+	wi::gui::Label infoLabel;
 	wi::gui::CheckBox realTimeCheckBox;
 	wi::gui::CheckBox msaaCheckBox;
 	wi::gui::Button generateButton;
