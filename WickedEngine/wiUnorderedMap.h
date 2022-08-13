bfa15d73a05491736a3da07b5e2c061b80e7bd2c
SeleDreams <seledreams.contact@gmail.com>
2022-08-13 15:22:41 +0200

    replaced default unordered_map to fix linux crash

--- WickedEngine/wiUnorderedMap.h
+++ WickedEngine/wiUnorderedMap.h
@@ -3,23 +3,32 @@
 // This file is used to allow replacement of std::unordered_map
 
 #ifndef WI_UNORDERED_MAP_TYPE
-#define WI_UNORDERED_MAP_TYPE 1
+#define WI_UNORDERED_MAP_TYPE 2
 #endif // WI_UNORDERED_MAP_TYPE
 
 #if WI_UNORDERED_MAP_TYPE == 1
 #include "Utility/flat_hash_map.hpp"
+#elif WI_UNORDERED_MAP_TYPE == 2
+#include "Utility/unordered_map.hpp"
+#elif WI_UNORDERED_MAP_TYPE == 3
+#include "Utility/bytell_hash_map.hpp"
 #else
 #include <unordered_map>
 #endif // WI_UNORDERED_MAP_TYPE
 
-namespace wi
-{
-	template<typename K, typename V, typename H = std::hash<K>, typename E = std::equal_to<K>, typename A = std::allocator<std::pair<const K, V> > >
+namespace wi {
+template <typename K, typename V, typename H = std::hash<K>,
+          typename E = std::equal_to<K>,
+          typename A = std::allocator<std::pair<const K, V>>>
 #if WI_UNORDERED_MAP_TYPE == 1
-	using unordered_map = ska::flat_hash_map<K, V, H, E, A>;
+using unordered_map = ska::flat_hash_map<K, V, H, E, A>;
+#elif WI_UNORDERED_MAP_TYPE == 2
+using unordered_map = ska::unordered_map<K, V, H, E, A>;
+#elif WI_UNORDERED_MAP_TYPE == 3
+using unordered_map = ska::bytell_hash_map<K, V, H, E, A>;
 #else
-	using unordered_map = std::unordered_map<K, V, H, E, A>;
+using unordered_map = std::unordered_map<K, V, H, E, A>;
 #endif // WI_UNORDERED_MAP_TYPE
-}
+} // namespace wi
 
 #endif // WI_UNORDERED_MAP_REPLACEMENT
