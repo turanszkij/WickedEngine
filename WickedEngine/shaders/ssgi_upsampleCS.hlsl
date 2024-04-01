0489e318c4b21cb89205db1d9e034cbbcd0a87bb
Turánszki János <turanszkij@users.noreply.github.com>
2024-04-01 16:01:36 +0200

    upsample improvement

--- WickedEngine/shaders/ssgi_upsampleCS.hlsl
+++ WickedEngine/shaders/ssgi_upsampleCS.hlsl
@@ -13,17 +13,28 @@ Texture2D<float2> input_normal_high : register(t4);
 RWTexture2D<float4> output : register(u0);
 
 static const float depthThreshold = 0.1;
-static const float normalThreshold = 1.0;
+static const float normalThreshold = 64;
 
 [numthreads(POSTPROCESS_BLOCKSIZE, POSTPROCESS_BLOCKSIZE, 1)]
-void main(uint2 DTid : SV_DispatchThreadID)
+void main(uint2 Gid : SV_GroupID, uint groupIndex : SV_GroupIndex)
 {
-	uint2 pixel = DTid.xy;
+	uint2 GTid = remap_lane_8x8(groupIndex);
+	uint2 pixel = Gid * POSTPROCESS_BLOCKSIZE + GTid;
 	const float2 uv = (pixel + 0.5) * postprocess.resolution_rcp;
 	
 	const float depth = input_depth_high[pixel];
 	const float linearDepth = compute_lineardepth(depth);
 	const float3 N = decode_oct(input_normal_high[pixel].rg);
+
+#if 1
+	const float3 P = reconstruct_position(uv, depth);
+	const float3 ddxP = P - QuadReadAcrossX(P);
+	const float3 ddyP = P - QuadReadAcrossY(P);
+	const float curve = saturate(1 - pow(1 - max(dot(ddxP, ddxP), dot(ddyP, ddyP)), 32));
+	const float normalPow = lerp(normalThreshold, 1, curve);
+#else
+	const float normalPow = normalThreshold;
+#endif
 
 	float3 result = 0;
 	float sum = 0;
@@ -48,8 +59,8 @@ void main(uint2 DTid : SV_DispatchThreadID)
 			float bilateralDepthWeight = 1 - saturate(abs(sampleLinearDepth - linearDepth) * depthThreshold);
 
 			const float3 sampleN = decode_oct(input_normal_low.SampleLevel(sampler_linear_clamp, sample_uv, 0));
-			float normalError = pow(saturate(dot(sampleN, N)), 4.0);
-			float bilateralNormalWeight = saturate(1.0 - (1.0 - normalError) * normalThreshold);
+			float normalError = pow(saturate(dot(sampleN, N)), normalPow);
+			float bilateralNormalWeight = normalError;
 
 			float weight = bilateralDepthWeight * bilateralNormalWeight;
 			
@@ -66,5 +77,6 @@ void main(uint2 DTid : SV_DispatchThreadID)
 	
 	result = max(0, result);
 	
-	output[pixel] = (output[pixel] + float4(result, 1)) ;
+	output[pixel] = output[pixel] + float4(result, 1);
+	//output[pixel] = float4(curve.xxx, 1);
 }
