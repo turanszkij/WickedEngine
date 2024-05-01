3cf1290df9a51a14aa32ed26de2659105e354bf4
Turánszki János <turanszkij@users.noreply.github.com>
2024-05-01 10:18:59 +0200

    textures, armatures and stuff

--- Editor/ModelImporter_FBX.cpp
+++ Editor/ModelImporter_FBX.cpp
@@ -18,6 +18,9 @@ void ImportModel_FBX(const std::string& filename, wi::scene::Scene& scene)
 	opts.target_unit_meters = 1.0f;
 	opts.target_camera_axes = ufbx_axes_left_handed_y_up;
 	opts.target_light_axes = ufbx_axes_left_handed_y_up;
+	opts.space_conversion = UFBX_SPACE_CONVERSION_TRANSFORM_ROOT;
+	//opts.space_conversion = UFBX_SPACE_CONVERSION_ADJUST_TRANSFORMS;
+	//opts.space_conversion = UFBX_SPACE_CONVERSION_MODIFY_GEOMETRY;
 	ufbx_error error = {};
 	ufbx_scene* fbxscene = ufbx_load_file(filename.c_str(), &opts, &error);
 	if (fbxscene == nullptr)
@@ -30,33 +33,98 @@ void ImportModel_FBX(const std::string& filename, wi::scene::Scene& scene)
 	}
 
 	wi::unordered_map<const ufbx_material*, Entity> material_lookup;
+	wi::unordered_map<const ufbx_skin_deformer*, Entity> skin_lookup;
 	wi::unordered_map<const ufbx_mesh*, Entity> mesh_lookup;
 	wi::unordered_map<const ufbx_node*, Entity> node_lookup;
 
-	Entity rootEntity = INVALID_ENTITY;
+	Entity rootEntity = CreateEntity();
+	{
+		scene.names.Create(rootEntity) = wi::helper::GetFileNameFromPath(filename);
+		const ufbx_node* node = fbxscene->root_node;
+		node_lookup[node] = rootEntity;
+		TransformComponent& transform = scene.transforms.Create(rootEntity);
+		transform.scale_local.x = node->local_transform.scale.x;
+		transform.scale_local.y = node->local_transform.scale.y;
+		transform.scale_local.z = node->local_transform.scale.z;
+		transform.rotation_local.x = node->local_transform.rotation.x;
+		transform.rotation_local.y = node->local_transform.rotation.y;
+		transform.rotation_local.z = node->local_transform.rotation.z;
+		transform.rotation_local.w = node->local_transform.rotation.w;
+		transform.translation_local.x = node->local_transform.translation.x;
+		transform.translation_local.y = node->local_transform.translation.y;
+		transform.translation_local.z = node->local_transform.translation.z;
+	}
 
 	for (const ufbx_material* material : fbxscene->materials)
 	{
 		Entity entity = CreateEntity();
+		scene.Component_Attach(entity, rootEntity);
 		material_lookup[material] = entity;
 		if (material->name.length > 0)
 		{
 			scene.names.Create(entity) = material->name.data;
 		}
 		MaterialComponent& materialcomponent = scene.materials.Create(entity);
-		materialcomponent.baseColor = XMFLOAT4(
-			material->pbr.base_color.value_vec4.x,
-			material->pbr.base_color.value_vec4.y,
-			material->pbr.base_color.value_vec4.z,
-			material->pbr.base_color.value_vec4.w
-		);
-		materialcomponent.roughness = material->pbr.roughness.value_real;
+		materialcomponent.SetDoubleSided(material->features.double_sided.enabled);
+		materialcomponent.baseColor = XMFLOAT4(material->pbr.base_color.value_vec4.v);
+		if (material->pbr.base_color.texture != nullptr)
+		{
+			materialcomponent.textures[MaterialComponent::BASECOLORMAP].name = material->pbr.base_color.texture->filename.data;
+		}
+		if (material->pbr.normal_map.texture != nullptr)
+		{
+			materialcomponent.textures[MaterialComponent::NORMALMAP].name = material->pbr.normal_map.texture->filename.data;
+		}
+		if (material->fbx.specular_factor.texture != nullptr) // not from pbr.
+		{
+			materialcomponent.textures[MaterialComponent::SPECULARMAP].name = material->fbx.specular_factor.texture->filename.data;
+		}
+		if (material->pbr.displacement_map.texture != nullptr)
+		{
+			materialcomponent.textures[MaterialComponent::DISPLACEMENTMAP].name = material->pbr.displacement_map.texture->filename.data;
+		}
+		if (material->pbr.ambient_occlusion.texture != nullptr)
+		{
+			materialcomponent.textures[MaterialComponent::OCCLUSIONMAP].name = material->pbr.ambient_occlusion.texture->filename.data;
+		}
+
+		if (
+			material->shader_type == UFBX_SHADER_3DS_MAX_PBR_METAL_ROUGH ||
+			material->shader_type == UFBX_SHADER_GLTF_MATERIAL
+			)
+		{
+			materialcomponent.roughness = material->pbr.roughness.value_real;
+			materialcomponent.metalness = material->pbr.metalness.value_real;
+			materialcomponent.reflectance = material->pbr.specular_ior.value_real;
+		}
+
+		if (
+			material->shader_type == UFBX_SHADER_3DS_MAX_PBR_SPEC_GLOSS
+			)
+		{
+			materialcomponent.SetUseSpecularGlossinessWorkflow(true);
+		}
+
 		materialcomponent.CreateRenderData();
+	}
+
+	for (const ufbx_skin_deformer* skin : fbxscene->skin_deformers)
+	{
+		Entity entity = CreateEntity();
+		scene.Component_Attach(entity, rootEntity);
+		skin_lookup[skin] = entity;
+		if (skin->name.length > 0)
+		{
+			scene.names.Create(entity) = skin->name.data;
+		}
+		scene.armatures.Create(entity);
+		scene.transforms.Create(entity);
 	}
 
 	for (const ufbx_mesh* mesh : fbxscene->meshes)
 	{
 		Entity entity = CreateEntity();
+		scene.Component_Attach(entity, rootEntity);
 		mesh_lookup[mesh] = entity;
 		if (mesh->name.length > 0)
 		{
@@ -64,10 +132,23 @@ void ImportModel_FBX(const std::string& filename, wi::scene::Scene& scene)
 		}
 		MeshComponent& meshcomponent = scene.meshes.Create(entity);
 		wi::vector<uint32_t> tri_indices(mesh->max_face_triangles * 3);
+		const ufbx_skin_deformer* skin = nullptr;
+		if (mesh->skin_deformers.count > 0)
+		{
+			skin = mesh->skin_deformers[0];
+			meshcomponent.armatureID = skin_lookup[skin];
+		}
 		for (const ufbx_mesh_part& part : mesh->material_parts)
 		{
 			wi::vector<XMFLOAT3> positions;
 			wi::vector<XMFLOAT3> normals;
+			wi::vector<XMFLOAT2> uvset0;
+			wi::vector<XMFLOAT2> uvset1;
+			wi::vector<wi::Color> colors;
+
+			wi::vector<XMUINT4> boneindices;
+			wi::vector<XMFLOAT4> boneweights;
+
 			for (uint32_t face_index : part.face_indices)
 			{
 				ufbx_face face = mesh->faces[face_index];
@@ -77,11 +158,53 @@ void ImportModel_FBX(const std::string& filename, wi::scene::Scene& scene)
 					uint32_t index = tri_indices[i];
 					if (mesh->vertex_position.exists)
 					{
-						positions.push_back(*(const XMFLOAT3*)mesh->vertex_position[index].v);
+						positions.push_back(XMFLOAT3(mesh->vertex_position[index].v));
 					}
 					if (mesh->vertex_normal.exists)
 					{
-						normals.push_back(*(const XMFLOAT3*)mesh->vertex_normal[index].v);
+						normals.push_back(XMFLOAT3(mesh->vertex_normal[index].v));
+					}
+					if (mesh->vertex_uv.exists)
+					{
+						uvset0.push_back(XMFLOAT2(mesh->vertex_uv[index].v));
+						uvset0.back().y = 1 - uvset0.back().y;
+					}
+					if (mesh->uv_sets.count > 1)
+					{
+						uvset1.push_back(XMFLOAT2(mesh->uv_sets[1].vertex_uv[index].v));
+						uvset1.back().y = 1 - uvset1.back().y;
+					}
+					if (mesh->vertex_color.exists)
+					{
+						colors.push_back(wi::Color::fromFloat4(XMFLOAT4(mesh->vertex_color[index].v)));
+					}
+
+					if (skin)
+					{
+						boneindices.emplace_back();
+						boneweights.emplace_back();
+
+						uint32_t vertex = mesh->vertex_indices[index];
+						ufbx_skin_vertex skin_vertex = skin->vertices[vertex];
+						uint32_t num_weights = skin_vertex.num_weights;
+						num_weights = std::min(num_weights, 4u);
+
+						float total_weight = 0;
+						for (uint32_t i = 0; i < num_weights; ++i)
+						{
+							ufbx_skin_weight skin_weight = skin->weights[skin_vertex.weight_begin + i];
+							(&boneindices.back().x)[i] = skin_weight.cluster_index;
+							(&boneweights.back().x)[i] = skin_weight.weight;
+							total_weight += skin_weight.weight;
+						}
+
+						if (total_weight > 0)
+						{
+							for (uint32_t i = 0; i < num_weights; ++i)
+							{
+								(&boneweights.back().x)[i] /= total_weight;
+							}
+						}
 					}
 				}
 			}
@@ -90,41 +213,93 @@ void ImportModel_FBX(const std::string& filename, wi::scene::Scene& scene)
 			wi::vector<uint32_t> indices;
 			indices.resize(part.num_triangles * 3);
 
-			ufbx_vertex_stream streams[] = {
-				{ positions.data(), positions.size(), sizeof(positions[0])},
-				{ normals.data(), normals.size(), sizeof(normals[0])},
-			};
+			wi::vector<ufbx_vertex_stream> streams;
+			if (!positions.empty())
+			{
+				streams.push_back({ positions.data(), positions.size(), sizeof(positions[0]) });
+			}
+			if (!normals.empty())
+			{
+				streams.push_back({ normals.data(), normals.size(), sizeof(normals[0]) });
+			}
+			if (!uvset0.empty())
+			{
+				streams.push_back({ uvset0.data(), uvset0.size(), sizeof(uvset0[0]) });
+			}
+			if (!uvset1.empty())
+			{
+				streams.push_back({ uvset1.data(), uvset1.size(), sizeof(uvset1[0]) });
+			}
+			if (!colors.empty())
+			{
+				streams.push_back({ colors.data(), colors.size(), sizeof(colors[0]) });
+			}
+			if (!boneindices.empty())
+			{
+				streams.push_back({ boneindices.data(), boneindices.size(), sizeof(boneindices[0]) });
+			}
+			if (!boneweights.empty())
+			{
+				streams.push_back({ boneweights.data(), boneweights.size(), sizeof(boneweights[0]) });
+			}
 
-			size_t num_vertices = ufbx_generate_indices(streams, arraysize(streams), indices.data(), indices.size(), nullptr, nullptr);
-
-			positions.resize(num_vertices);
-			normals.resize(num_vertices);
+			const size_t num_vertices = ufbx_generate_indices(streams.data(), streams.size(), indices.data(), indices.size(), nullptr, nullptr);
 
 			MeshComponent::MeshSubset& subset = meshcomponent.subsets.emplace_back();
 			subset.indexOffset = (uint32_t)meshcomponent.indices.size();
 			subset.indexCount = uint32_t(indices.size());
-			subset.materialID = scene.materials.GetEntity(0);
+			subset.materialID = material_lookup[mesh->materials[part.index]];
 
 			meshcomponent.indices.insert(meshcomponent.indices.begin(), indices.begin(), indices.end());
-			meshcomponent.vertex_positions.insert(meshcomponent.vertex_positions.end(), positions.begin(), positions.end());
-			meshcomponent.vertex_normals.insert(meshcomponent.vertex_normals.end(), normals.begin(), normals.end());
 
+			if (!positions.empty())
+			{
+				positions.resize(num_vertices);
+				meshcomponent.vertex_positions.insert(meshcomponent.vertex_positions.end(), positions.begin(), positions.end());
+			}
+			if (!normals.empty())
+			{
+				normals.resize(num_vertices);
+				meshcomponent.vertex_normals.insert(meshcomponent.vertex_normals.end(), normals.begin(), normals.end());
+			}
+			if (!uvset0.empty())
+			{
+				uvset0.resize(num_vertices);
+				meshcomponent.vertex_uvset_0.insert(meshcomponent.vertex_uvset_0.end(), uvset0.begin(), uvset0.end());
+			}
+			if (!uvset1.empty())
+			{
+				uvset1.resize(num_vertices);
+				meshcomponent.vertex_uvset_1.insert(meshcomponent.vertex_uvset_1.end(), uvset1.begin(), uvset1.end());
+			}
+			if (!colors.empty())
+			{
+				colors.resize(num_vertices);
+				meshcomponent.vertex_colors.insert(meshcomponent.vertex_colors.end(), colors.begin(), colors.end());
+			}
+			if (!boneindices.empty())
+			{
+				boneindices.resize(num_vertices);
+				meshcomponent.vertex_boneindices.insert(meshcomponent.vertex_boneindices.end(), boneindices.begin(), boneindices.end());
+			}
+			if (!boneweights.empty())
+			{
+				boneweights.resize(num_vertices);
+				meshcomponent.vertex_boneweights.insert(meshcomponent.vertex_boneweights.end(), boneweights.begin(), boneweights.end());
+			}
 		}
 		meshcomponent.CreateRenderData();
 	}
 
 	for (const ufbx_node* node : fbxscene->nodes)
 	{
+		if (node->is_root)
+			continue;
 		Entity entity = CreateEntity();
 		node_lookup[node] = entity;
 		if (node->name.length > 0)
 		{
 			scene.names.Create(entity) = node->name.data;
-		}
-
-		if (node->is_root)
-		{
-			rootEntity = entity;
 		}
 
 		TransformComponent& transform = scene.transforms.Create(entity);
@@ -144,17 +319,99 @@ void ImportModel_FBX(const std::string& filename, wi::scene::Scene& scene)
 			ObjectComponent& object = scene.objects.Create(entity);
 			object.meshID = mesh_lookup[node->mesh];
 		}
+		if (node->light)
+		{
+			LightComponent& light = scene.lights.Create(entity);
+			light.SetCastShadow(node->light->cast_shadows);
+			light.color = XMFLOAT3(node->light->color.v);
+			light.direction = XMFLOAT3(node->light->local_direction.v);
+			light.innerConeAngle = node->light->inner_angle;
+			light.outerConeAngle = node->light->outer_angle;
+			switch (node->light->type)
+			{
+			default:
+			case UFBX_LIGHT_POINT:
+				light.SetType(LightComponent::LightType::POINT);
+				break;
+			case UFBX_LIGHT_SPOT:
+				light.SetType(LightComponent::LightType::SPOT);
+				break;
+			case UFBX_LIGHT_DIRECTIONAL:
+				light.SetType(LightComponent::LightType::DIRECTIONAL);
+				break;
+			}
+		}
+		if (node->camera)
+		{
+			CameraComponent& camera = scene.cameras.Create(entity);
+			camera.zNearP = node->camera->near_plane * 0.01f;
+			camera.zFarP = node->camera->far_plane * 0.01f;
+			camera.fov = wi::math::DegreesToRadians(node->camera->field_of_view_deg.y);
+		}
 	}
 
 	// After all nodes were iterated, determine hierarchy:
-	for (size_t i = 0; i < fbxscene->nodes.count; i++)
+	for (const ufbx_node* node : fbxscene->nodes)
 	{
-		ufbx_node* node = fbxscene->nodes.data[i];
+		if (node->is_root)
+			continue;
+		Entity entity = node_lookup[node];
 		if (node->parent)
 		{
-			Entity entity = node_lookup[node];
 			Entity parent = node_lookup[node->parent];
 			scene.Component_Attach(entity, parent, true);
+		}
+		else
+		{
+			scene.Component_Attach(entity, rootEntity, true);
+		}
+	}
+
+	// After nodes are complete, fill bones:
+	for (const ufbx_skin_deformer* skin : fbxscene->skin_deformers)
+	{
+		Entity entity = skin_lookup[skin];
+		ArmatureComponent* armature = scene.armatures.GetComponent(entity);
+		if (armature == nullptr)
+			continue;
+		for (const ufbx_skin_cluster* cluster : skin->clusters)
+		{
+			armature->boneCollection.push_back(node_lookup[cluster->bone_node]);
+
+			XMFLOAT4X4 inverseBindMatrix = wi::math::IDENTITY_MATRIX;
+			inverseBindMatrix._11 = cluster->geometry_to_bone.m00;
+			inverseBindMatrix._12 = cluster->geometry_to_bone.m10;
+			inverseBindMatrix._13 = cluster->geometry_to_bone.m20;
+
+			inverseBindMatrix._21 = cluster->geometry_to_bone.m01;
+			inverseBindMatrix._22 = cluster->geometry_to_bone.m11;
+			inverseBindMatrix._23 = cluster->geometry_to_bone.m21;
+
+			inverseBindMatrix._31 = cluster->geometry_to_bone.m02;
+			inverseBindMatrix._32 = cluster->geometry_to_bone.m12;
+			inverseBindMatrix._33 = cluster->geometry_to_bone.m22;
+
+			inverseBindMatrix._41 = cluster->geometry_to_bone.m03;
+			inverseBindMatrix._42 = cluster->geometry_to_bone.m13;
+			inverseBindMatrix._43 = cluster->geometry_to_bone.m23;
+
+			armature->inverseBindMatrices.push_back(inverseBindMatrix);
+		}
+	}
+
+	for (const ufbx_anim_stack* anim_stack : fbxscene->anim_stacks)
+	{
+		for (const ufbx_anim_layer* layer : anim_stack->layers)
+		{
+			Entity entity = CreateEntity();
+			scene.Component_Attach(entity, rootEntity);
+			if (layer->name.length > 0)
+			{
+				scene.names.Create(entity) = layer->name.data;
+			}
+			AnimationComponent& animcomponent = scene.animations.Create(entity);
+			animcomponent.start = (float)layer->anim->time_begin;
+			animcomponent.end = (float)layer->anim->time_end;
 		}
 	}
 
