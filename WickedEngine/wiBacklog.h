b1155c823ed9522133c88939293ae31d3002fa5b
Turánszki János <turanszkij@users.noreply.github.com>
2025-01-02 15:53:28 +0100

    try linux fix

--- WickedEngine/wiBacklog.h
+++ WickedEngine/wiBacklog.h
@@ -8,12 +8,12 @@
 #include <string>
 #include <cassert>
 
-#define wilog_level(str,level,...) {char text[1024] = {}; snprintf(text, sizeof(text), str, __VA_ARGS__); wi::backlog::post(text, level);}
-#define wilog_messagebox(str,...) {char text[1024] = {}; snprintf(text, sizeof(text), str, __VA_ARGS__); wi::backlog::post(text, wi::backlog::LogLevel::Error); wi::helper::messageBox(text, "Error!");}
-#define wilog_warning(str,...) {wilog_level(str, wi::backlog::LogLevel::Warning, __VA_ARGS__);}
-#define wilog_error(str,...) {wilog_level(str, wi::backlog::LogLevel::Error, __VA_ARGS__);}
-#define wilog(str,...) {wilog_level(str, wi::backlog::LogLevel::Default, __VA_ARGS__);}
-#define wilog_assert(cond,str,...) {if(!cond){wilog_error(str, __VA_ARGS__); assert(cond);}}
+#define wilog_level(str,level,...) {char text[1024] = {}; snprintf(text, sizeof(text), str, ## __VA_ARGS__); wi::backlog::post(text, level);}
+#define wilog_messagebox(str,...) {char text[1024] = {}; snprintf(text, sizeof(text), str, ## __VA_ARGS__); wi::backlog::post(text, wi::backlog::LogLevel::Error); wi::helper::messageBox(text, "Error!");}
+#define wilog_warning(str,...) {wilog_level(str, wi::backlog::LogLevel::Warning, ## __VA_ARGS__);}
+#define wilog_error(str,...) {wilog_level(str, wi::backlog::LogLevel::Error, ## __VA_ARGS__);}
+#define wilog(str,...) {wilog_level(str, wi::backlog::LogLevel::Default, ## __VA_ARGS__);}
+#define wilog_assert(cond,str,...) {if(!cond){wilog_error(str, ## __VA_ARGS__); assert(cond);}}
 
 namespace wi::backlog
 {
