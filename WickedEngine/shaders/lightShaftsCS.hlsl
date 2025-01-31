745e19a4769bf4571acc3907e59070cf7b2c8d7b
Turánszki János <turanszkij@users.noreply.github.com>
2025-01-31 07:32:43 +0100

    lightshafts downsampling with AA

--- WickedEngine/shaders/lightShaftsCS.hlsl
+++ WickedEngine/shaders/lightShaftsCS.hlsl
@@ -3,40 +3,34 @@
 
 PUSHCONSTANT(postprocess, PostProcess);
 
-Texture2D<float4> input : register(t0);
+Texture2D<half4> input : register(t0);
 
-RWTexture2D<float4> output : register(u0);
+RWTexture2D<half4> output : register(u0);
 
-static const uint NUM_SAMPLES = 32;
-static const uint UNROLL_GRANULARITY = 8;
+static const uint NUM_SAMPLES = 64;
 
 [numthreads(POSTPROCESS_BLOCKSIZE, POSTPROCESS_BLOCKSIZE, 1)]
 void main(uint3 DTid : SV_DispatchThreadID)
 {
-	float2 uv = (DTid.xy + 0.5f) * postprocess.resolution_rcp;
+	float2 uv = (DTid.xy + 0.5) * postprocess.resolution_rcp;
 
-	float3 color = input.SampleLevel(sampler_linear_clamp, uv, 0).rgb;
+	half3 color = input.SampleLevel(sampler_linear_clamp, uv, 0).rgb;
 
 	float2 lightPos = postprocess.params1.xy;
 	float2 deltaTexCoord =  uv - lightPos;
 	deltaTexCoord *= postprocess.params0.x / NUM_SAMPLES;
-	float illuminationDecay = 1.0f;
+	half illuminationDecay = 1.0;
 	
-	[loop] // loop big part (balance register pressure)
-	for (uint i = 0; i < NUM_SAMPLES / UNROLL_GRANULARITY; i++)
+	for (uint i = 0; i < NUM_SAMPLES; i++)
 	{
-		[unroll] // unroll small parts (balance register pressure)
-		for (uint j = 0; j < UNROLL_GRANULARITY; ++j)
-		{
-			uv.xy -= deltaTexCoord;
-			float3 sam = input.SampleLevel(sampler_linear_clamp, uv.xy, 0).rgb;
-			sam *= illuminationDecay * postprocess.params0.y;
-			color.rgb += sam;
-			illuminationDecay *= postprocess.params0.z;
-		}
+		uv.xy -= deltaTexCoord;
+		half3 sam = input.SampleLevel(sampler_linear_clamp, uv.xy, 0).rgb;
+		sam *= illuminationDecay * postprocess.params0.y;
+		color.rgb += sam;
+		illuminationDecay *= postprocess.params0.z;
 	}
 
 	color *= postprocess.params0.w;
 
-	output[DTid.xy] = float4(color, 1);
+	output[DTid.xy] = half4(color, 1);
 }
