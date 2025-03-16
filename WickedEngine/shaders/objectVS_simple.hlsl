8b9e2ce6e17666edebfe6e4ce0e51332753335fd
Turánszki János <turanszkij@users.noreply.github.com>
2025-03-16 06:45:35 +0100

    debug shader tessellation compatibility fix #1067
    Related Work Items: #10

--- WickedEngine/shaders/objectVS_simple.hlsl
+++ WickedEngine/shaders/objectVS_simple.hlsl
@@ -1,4 +1,5 @@
 #define OBJECTSHADER_COMPILE_VS
 #define OBJECTSHADER_LAYOUT_SHADOW_TEX
 #define OBJECTSHADER_USE_COLOR
+#define OBJECTSHADER_USE_NORMAL // tessellation compat!
 #include "objectHF.hlsli"
