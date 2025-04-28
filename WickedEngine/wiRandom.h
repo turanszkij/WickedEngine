e77e95660994bd0de070945dcff27a746c2e3dfb
Turánszki János <turanszkij@users.noreply.github.com>
2025-04-28 17:12:44 +0200

    wasnt it

--- WickedEngine/wiRandom.h
+++ WickedEngine/wiRandom.h
@@ -11,31 +11,31 @@ namespace wi::random
 	{
 		uint64_t state = 0;
 
-		inline RNG(uint64_t seed = 0) : state(seed) {}
+		constexpr RNG(uint64_t seed = 0) : state(seed) {}
 
 		// seeds the random number generator, seed should be non-zero number
-		inline void seed(uint64_t seed)
+		constexpr void seed(uint64_t seed)
 		{
 			state = seed;
 		}
 
 		// gives an uint in range [0, UINT64_MAX]
-		inline uint64_t next_uint()
+		constexpr uint64_t next_uint()
 		{
-			state ^= state >> 12;
-			state ^= state << 25;
-			state ^= state >> 27;
+			state ^= state >> 12ull;
+			state ^= state << 25ull;
+			state ^= state >> 27ull;
 			return state * 0x2545F4914F6CDD1DULL;
 		}
 		// gives an uint64 in range [min, max]
-		inline uint64_t next_uint(uint64_t min, uint64_t max)
+		constexpr uint64_t next_uint(uint64_t min, uint64_t max)
 		{
 			if (min == max)
 				return min;
 			return min + (next_uint() % (std::min(std::numeric_limits<uint64_t>::max() - uint64_t(1), std::max(uint64_t(1), max - min)) + uint64_t(1)));
 		}
 		// gives an uint32 in range [min, max]
-		inline uint32_t next_uint(uint32_t min, uint32_t max)
+		constexpr uint32_t next_uint(uint32_t min, uint32_t max)
 		{
 			if (min == max)
 				return min;
@@ -43,20 +43,25 @@ namespace wi::random
 		}
 
 		// gives an int64 in range [-INT64_MAX, INT64_MAX]
-		inline int64_t next_int()
+		constexpr int64_t next_int()
 		{
-			uint64_t u = next_uint();
-			return *(int64_t*)&u;
+			union
+			{
+				uint64_t u;
+				int64_t i;
+			} value = {};
+			value.u = next_uint();
+			return value.i;
 		}
 		// gives an int64 in range [min, max]
-		inline int64_t next_int(int64_t min, int64_t max)
+		constexpr int64_t next_int(int64_t min, int64_t max)
 		{
 			if (min == max)
 				return min;
 			return min + int64_t(next_uint() % (std::min(std::numeric_limits<int64_t>::max() - int64_t(1), std::max(int64_t(1), max - min)) + int64_t(1))); // we roll next_uint here to avoid negative value messing with range mapping
 		}
 		// gives an int32 in range [min, max]
-		inline int32_t next_int(int32_t min, int32_t max)
+		constexpr int32_t next_int(int32_t min, int32_t max)
 		{
 			if (min == max)
 				return min;
@@ -64,13 +69,18 @@ namespace wi::random
 		}
 
 		// gives a float in range [0, 1]
-		inline float next_float()
+		constexpr float next_float()
 		{
-			uint32_t u = 0x3f800000 | (uint32_t(next_uint()) >> 9);
-			return *((float*)&u) - 1.0f;
+			union
+			{
+				uint32_t u;
+				float f;
+			} value = {};
+			value.u = 0x3f800000u | (uint32_t(next_uint()) >> 9);
+			return value.f - 1.0f;
 		}
 		// gives a float in range [min, max]
-		inline float next_float(float min, float max)
+		constexpr float next_float(float min, float max)
 		{
 			return min + (max - min) * next_float();
 		}
