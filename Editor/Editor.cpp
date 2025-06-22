1fc1fd1808e2b4845a837cd169a83de45ff2b96b
ADoseOfCode <140978248+AnkeDoesCoding@users.noreply.github.com>
2025-06-22 12:07:52 +0100

    WIP rename entity shortcut

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -67,6 +67,7 @@ enum class EditorActions
 	SELECT_ALL_ENTITIES,
 	DESELECT_ALL_ENTITIES,
 	FOCUS_ON_SELECTION,
+	RENAME_ENTITY,
 
 	// Edit actions
 	UNDO_ACTION,
@@ -126,6 +127,7 @@ HotkeyInfo hotkeyActions[size_t(EditorActions::COUNT)] = {
 	{wi::input::BUTTON('A'),					/*press=*/ true,		/*control=*/ true,		/*shift=*/ false},	//SELECT_ALL_ENTITIES,
 	{wi::input::BUTTON::KEYBOARD_BUTTON_ESCAPE,	/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//DESELECT_ALL_ENTITIES,
 	{wi::input::BUTTON('F'),					/*press=*/ false,		/*control=*/ false,		/*shift=*/ false},	//FOCUS_ON_SELECTION,
+	{wi::input::BUTTON::KEYBOARD_BUTTON_F2,		/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//RENAME_ENTITY,
 	{wi::input::BUTTON('Z'),					/*press=*/ true,		/*control=*/ true,		/*shift=*/ false},	//UNDO_ACTION,
 	{wi::input::BUTTON('Y'),					/*press=*/ true,		/*control=*/ true,		/*shift=*/ false},	//REDO_ACTION,
 	{wi::input::BUTTON('C'),					/*press=*/ true,		/*control=*/ true,		/*shift=*/ false},	//COPY_ACTION,
@@ -135,7 +137,7 @@ HotkeyInfo hotkeyActions[size_t(EditorActions::COUNT)] = {
 	{wi::input::BUTTON('1'),					/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//MOVE_TOGGLE_ACTION,
 	{wi::input::BUTTON('2'),					/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//ROTATE_TOGGLE_ACTION,
 	{wi::input::BUTTON('3'),					/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//SCALE_TOGGLE_ACTION,
-	{wi::input::BUTTON::KEYBOARD_BUTTON_F2,		/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//MAKE_NEW_SCREENSHOT,
+	{wi::input::BUTTON('L'),					/*press=*/ true,		/*control=*/ false,		/*shift=*/ false},	//MAKE_NEW_SCREENSHOT,
 	{wi::input::BUTTON('I'),					/*press=*/ false,		/*control=*/ false,		/*shift=*/ false},	//INSPECTOR_MODE,
 	{wi::input::BUTTON::MOUSE_BUTTON_LEFT,		/*press=*/ true,		/*control=*/ true,		/*shift=*/ true},	//PLACE_INSTANCES,
 	{wi::input::BUTTON('S'),					/*press=*/ true,		/*control=*/ true,		/*shift=*/ true},	//SAVE_SCENE_AS,
@@ -199,6 +201,7 @@ void HotkeyRemap(Editor* main)
 		{"SELECT_ALL_ENTITIES", EditorActions::SELECT_ALL_ENTITIES},
 		{"DESELECT_ALL_ENTITIES", EditorActions::DESELECT_ALL_ENTITIES},
 		{"FOCUS_ON_SELECTION", EditorActions::FOCUS_ON_SELECTION},
+		{"RENAME_ENTITY", EditorActions::RENAME_ENTITY},
 		{"UNDO_ACTION", EditorActions::UNDO_ACTION},
 		{"REDO_ACTION", EditorActions::REDO_ACTION},
 		{"COPY_ACTION", EditorActions::COPY_ACTION},
@@ -1798,6 +1801,18 @@ void EditorComponent::Update(float dt)
 		if (!translator.selected.empty() && CheckInput(EditorActions::FOCUS_ON_SELECTION))
 		{
 			FocusCameraOnSelected();
+		}
+
+		if(!translator.selected.empty() && CheckInput(EditorActions::RENAME_ENTITY))
+		{
+			for (auto& x : translator.selected)
+			{
+				if (NameComponent* name_comp = scene.names.GetComponent(x.entity))
+				{
+					componentsWnd.nameWnd.nameInput.SetAsActive();
+					componentsWnd.nameWnd.SetCollapsed(false);
+				}
+			}
 		}
 
 		inspector_mode = CheckInput(EditorActions::INSPECTOR_MODE);
