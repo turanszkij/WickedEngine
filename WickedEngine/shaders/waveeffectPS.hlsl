cdeaa7d1ad87ec43b258c197c1e5aee3c9b43bee
Turánszki János <turanszkij@gmail.com>
2025-07-06 14:11:18 +0200

    u

--- WickedEngine/shaders/waveeffectPS.hlsl
+++ WickedEngine/shaders/waveeffectPS.hlsl
@@ -13,12 +13,12 @@ float4 main(in float4 pos : SV_Position, in float2 uv_screen : TEXCOORD) : SV_Ta
 	float waves = 0;
 	for (uint i = 0; i < 6; i++)
 	{
-		uv.y += (0.2 * sin(uv.x * 4 + i * 2 - iTime * 0.1));
-		float li = 0.025 + smoothstep(0.0, 0.8, pow(abs(fmod(uv_screen.x + i / 1.1 + iTime, 2.0) - 1.0), 2.0));
+		uv.y += (0.1 * sin(uv.x * 4 + i * 2 - iTime * 0.1));
+		float li = 0.025 + smoothstep(0.0, 0.8, pow(abs(fmod(uv_screen.x + i * 0.1 + iTime, 2.0) - 1.0), 2.0));
 		float gw = li / (32.0 * uv.y);
 		float sgw = sign(gw) * (i % 2 == 0 ? 1 : -1);
 		gw = abs(gw);
-		gw = smoothstep(sgw < 0 ? 0.2 : 0.0, 0.3, gw);
+		gw = smoothstep(sgw < 0 ? 0.2 : 0.0, 0.6, gw);
 		float ts = gw * (sin(iTime * 4) * 0.5 + 0.5 + 1.4);
 		waves += ts;
 	}
