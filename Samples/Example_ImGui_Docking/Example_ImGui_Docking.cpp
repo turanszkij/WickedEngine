011e29defe5bea08f5b92b2982da0ae08e9dc38f
Dennis Brakhane <brakhane@gmail.com>
2025-09-26 16:49:56 +0200

    example_imgui_docking: rename confusingly named DIVTWOPI to DEG_PER_RAD

--- Samples/Example_ImGui_Docking/Example_ImGui_Docking.cpp
+++ Samples/Example_ImGui_Docking/Example_ImGui_Docking.cpp
@@ -408,8 +408,7 @@ void Example_ImGuiRenderer::Load()
 	RenderPath3D::Load();
 
 }
-
-#define DIVTWOPI 57.29577951307855
+#define DEG_PER_RAD 57.29577951307855
 #define IMGUIBUTTONWIDE 200.0f
 #define CAMERAMOVESPEED 10.0
 bool show_demo_window = false;
@@ -1163,7 +1162,7 @@ void Example_ImGuiRenderer::Update(float dt)
 			ImGui::SetCursorPos(ImGui::GetCursorPos() + ImVec2(0.0f, -3.0f));
 			ImGui::PushItemWidth(inputsize - ImGui::GetFontSize());
 			ImGui::InputFloat("##CamAngleX", &camera_ang[0], 0.0f, 0.0f, "%.2f");
-			if (ImGui::IsItemHovered()) ImGui::SetTooltip("Camera Angle X (Roll %.3f)", camera_ang[0] / DIVTWOPI);
+			if (ImGui::IsItemHovered()) ImGui::SetTooltip("Camera Angle X (Roll %.3f)", camera_ang[0] / DEG_PER_RAD);
 			ImGui::PopItemWidth();
 
 			ImGui::SameLine();
@@ -1173,7 +1172,7 @@ void Example_ImGuiRenderer::Update(float dt)
 			ImGui::SetCursorPos(ImGui::GetCursorPos() + ImVec2(0.0f, -3.0f));
 			ImGui::PushItemWidth(inputsize - ImGui::GetFontSize());
 			ImGui::InputFloat("##CamAngleY", &camera_ang[1], 0.0f, 0.0f, "%.2f");
-			if (ImGui::IsItemHovered()) ImGui::SetTooltip("Camera Angle Y (Pitch %.3f)", camera_ang[1] / DIVTWOPI);
+			if (ImGui::IsItemHovered()) ImGui::SetTooltip("Camera Angle Y (Pitch %.3f)", camera_ang[1] / DEG_PER_RAD);
 			ImGui::PopItemWidth();
 			ImGui::SameLine();
 
@@ -1183,7 +1182,7 @@ void Example_ImGuiRenderer::Update(float dt)
 			ImGui::SetCursorPos(ImGui::GetCursorPos() + ImVec2(0.0f, -3.0f));
 			ImGui::PushItemWidth(inputsize - ImGui::GetFontSize());
 			ImGui::InputFloat("##CamAngleZ", &camera_ang[2], 0.0f, 0.0f, "%.2f");
-			if (ImGui::IsItemHovered()) ImGui::SetTooltip("Camera Angle Z (Yaw %.3f)", camera_ang[2] / DIVTWOPI);
+			if (ImGui::IsItemHovered()) ImGui::SetTooltip("Camera Angle Z (Yaw %.3f)", camera_ang[2] / DEG_PER_RAD);
 			ImGui::PopItemWidth();
 
 			ImGui::PushItemWidth(w);
@@ -1540,7 +1539,7 @@ void Example_ImGuiRenderer::Update(float dt)
 
 	camera_transform.ClearTransform();
 	camera_transform.Translate(XMFLOAT3(camera_pos[0], camera_pos[1], camera_pos[2]));
-	camera_transform.RotateRollPitchYaw(XMFLOAT3(camera_ang[0] / (float) DIVTWOPI, camera_ang[1] / (float) DIVTWOPI, camera_ang[2] / (float) DIVTWOPI));
+	camera_transform.RotateRollPitchYaw(XMFLOAT3(camera_ang[0] / (float) DEG_PER_RAD, camera_ang[1] / (float) DEG_PER_RAD, camera_ang[2] / (float) DEG_PER_RAD));
 	camera_transform.UpdateTransform();
 	camera.TransformCamera(camera_transform);
 	camera.UpdateCamera();
