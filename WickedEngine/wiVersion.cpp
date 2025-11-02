a6adfc1284cffd640398312a4da1a42cba048b24
Turánszki János <turanszkij@gmail.com>
2025-11-02 08:49:37 +0100

    block allocator should respect alignment of items

--- WickedEngine/wiVersion.cpp
+++ WickedEngine/wiVersion.cpp
@@ -9,7 +9,7 @@ namespace wi::version
 	// minor features, major updates, breaking compatibility changes
 	const int minor = 71;
 	// minor bug fixes, alterations, refactors, updates
-	const int revision = 847;
+	const int revision = 848;
 
 	const std::string version_string = std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(revision);
 
