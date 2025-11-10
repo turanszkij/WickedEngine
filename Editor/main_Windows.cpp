5e1d74a68e8869c4b0fe0e6dda4048cb63566ece
nxrighthere <nxrighthere@gmail.com>
2025-11-10 15:20:52 +0100

    Fix the Alt key freezing the editor

--- Editor/main_Windows.cpp
+++ Editor/main_Windows.cpp
@@ -112,6 +112,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 				break;
 			}
 			break;
+		case WM_SYSKEYDOWN:
+		case WM_SYSKEYUP:
+			// Handle system key messages (like Alt key) to prevent menu bar activation freeze
+			// Return 0 to indicate we processed the message and prevent default behavior
+			break;
 		case WM_SETTINGCHANGE:
 			{
 				// Change window theme dark mode based on system setting:
