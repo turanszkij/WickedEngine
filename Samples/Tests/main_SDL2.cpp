e65d039673c13aae3d83ae77db234b0b474f362f
Turánszki János <turanszkij@gmail.com>
2025-11-15 10:58:49 +0100

    fixes

--- Samples/Tests/main_SDL2.cpp
+++ Samples/Tests/main_SDL2.cpp
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
 
     sdl2::sdlsystem_ptr_t system = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING | SDL_INIT_EVENTS);
     if (*system) {
-        throw sdl2::SDLError("Error creating SDL2 system");
+		wilog_assert("Error creating SDL2 system");
     }
 
     sdl2::window_ptr_t window = sdl2::make_window(
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
             1280, 800,
             SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     if (!window) {
-        throw sdl2::SDLError("Error creating window");
+		wilog_assert("Error creating window");
     }
 
     tests.SetWindow(window.get());
