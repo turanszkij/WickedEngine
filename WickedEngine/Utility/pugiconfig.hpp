c1e108605467412b665c2d97d99eda9c83334007
Turánszki János <turanszkij@gmail.com>
2025-11-15 10:37:24 +0100

    wrong pugiconfig, fix

--- WickedEngine/Utility/pugiconfig.hpp
+++ WickedEngine/Utility/pugiconfig.hpp
@@ -22,10 +22,10 @@
 // #define PUGIXML_NO_XPATH
 
 // Uncomment this to disable STL
-#define PUGIXML_NO_STL
+// #define PUGIXML_NO_STL
 
 // Uncomment this to disable exceptions
-// #define PUGIXML_NO_EXCEPTIONS
+#define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
 // #define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
