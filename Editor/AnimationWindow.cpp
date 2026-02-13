4c667481c89833138fc3a414430843c1496d2dcb
Stanislav Denisov <nxrighthere@gmail.com>
2026-02-13 06:55:00 +0100

    Fix pinning of editor component windows (#1552)

--- Editor/AnimationWindow.cpp
+++ Editor/AnimationWindow.cpp
@@ -1071,7 +1071,7 @@ void AnimationWindow::SetEntity(Entity entity)
 		wi::scene::Scene& scene = editor->GetCurrentScene();
 
 		AnimationComponent* animation = scene.animations.GetComponent(entity);
-		if (animation != nullptr || IsCollapsed())
+		if (animation != nullptr || (IsCollapsed() && entity == INVALID_ENTITY))
 		{
 			this->entity = entity;
 			RefreshKeyframesList();
