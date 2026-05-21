8851f834710051fcdeddb32d57d0f8192156b54e
orbisai0security <mediratta01.pally@gmail.com>
2026-05-21 12:13:10 +0000

    fix: V-003 security vulnerability
    Automated security fix generated by OrbisAI Security

--- WickedEngine/LUA/linit.c
+++ WickedEngine/LUA/linit.c
@@ -41,15 +41,11 @@
 */
 static const luaL_Reg loadedlibs[] = {
   {LUA_GNAME, luaopen_base},
-  {LUA_LOADLIBNAME, luaopen_package},
   {LUA_COLIBNAME, luaopen_coroutine},
   {LUA_TABLIBNAME, luaopen_table},
-  {LUA_IOLIBNAME, luaopen_io},
-  {LUA_OSLIBNAME, luaopen_os},
   {LUA_STRLIBNAME, luaopen_string},
   {LUA_MATHLIBNAME, luaopen_math},
   {LUA_UTF8LIBNAME, luaopen_utf8},
-  {LUA_DBLIBNAME, luaopen_debug},
   {NULL, NULL}
 };
 
