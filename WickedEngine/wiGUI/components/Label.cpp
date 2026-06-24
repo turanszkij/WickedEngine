fc9fbf5ed97da21d78e320684257c4117263cf66
Anthony Gaudino <anthonygaudino.45@gmail.com>
2026-06-24 10:43:28 +0100

    improve: Label.cpp
    Refactor Label.cpp for legibility and simplicity.
    No behavior change.

--- WickedEngine/wiGUI/components/Label.cpp
+++ WickedEngine/wiGUI/components/Label.cpp
@@ -3,24 +3,24 @@
  * Implementation of the @ref wi::gui::Label widget.
  */
 
-#include "wiGUI/components/Label.h"
-#include "wiGUI/GUIInternal.h"
+#include <iomanip> // setprecision
+#include <sstream>
+#include <utility>
 
+#include "wiBacklog.h"
+#include "wiEventHandler.h"
+#include "wiFont.h"
+#include "wiGUI/GUIInternal.h"
+#include "wiHelper.h"
+#include "wiImage.h"
 #include "wiInput.h"
 #include "wiPrimitive.h"
 #include "wiProfiler.h"
 #include "wiRenderer.h"
-#include "wiTimer.h"
-#include "wiEventHandler.h"
-#include "wiFont.h"
-#include "wiImage.h"
 #include "wiTextureHelper.h"
-#include "wiBacklog.h"
-#include "wiHelper.h"
+#include "wiTimer.h"
 
-#include <sstream>
-#include <iomanip> // setprecision
-#include <utility>
+#include "wiGUI/components/Label.h"
 
 using namespace wi::graphics;
 using namespace wi::primitive;
@@ -36,17 +36,19 @@ namespace wi::gui
 
 		scrollbar.SetColor(wi::Color(80, 80, 80, 100), wi::gui::IDLE);
 		scrollbar.sprites_knob[ScrollBar::SCROLLBAR_INACTIVE].params.color = wi::Color(140, 140, 140, 140);
-		scrollbar.sprites_knob[ScrollBar::SCROLLBAR_HOVER].params.color = wi::Color(180, 180, 180, 180);
-		scrollbar.sprites_knob[ScrollBar::SCROLLBAR_GRABBED].params.color = wi::Color::White();
+		scrollbar.sprites_knob[ScrollBar::SCROLLBAR_HOVER].params.color    = wi::Color(180, 180, 180, 180);
+		scrollbar.sprites_knob[ScrollBar::SCROLLBAR_GRABBED].params.color  = wi::Color::White();
 		scrollbar.SetOverScroll(0.25f);
 		scrollbar.knob_inset_border = XMFLOAT2(4, 2);
 	}
+
 	void Label::Update(const wi::Canvas& canvas, const float dt)
 	{
 		if (!IsVisible())
 		{
 			return;
 		}
+
 		Widget::Update(canvas, dt);
 
 		if (wrap_enabled)
@@ -60,36 +62,46 @@ namespace wi::gui
 
 		switch (font.params.h_align)
 		{
-		case wi::font::WIFALIGN_LEFT:
-			font.params.posX = translation.x + 2 + margin_left;
-			break;
-		case wi::font::WIFALIGN_RIGHT:
-			font.params.posX = translation.x + scale.x - 2 - margin_right;
-			break;
-		case wi::font::WIFALIGN_CENTER:
-		default:
-			font.params.posX = translation.x + scale.x * 0.5f;
-			break;
+			case wi::font::WIFALIGN_LEFT:
+				font.params.posX = translation.x + 2 + margin_left;
+
+				break;
+			case wi::font::WIFALIGN_RIGHT:
+				font.params.posX = translation.x + scale.x - 2 - margin_right;
+
+				break;
+			case wi::font::WIFALIGN_CENTER:
+			default:
+				font.params.posX = translation.x + scale.x * 0.5f;
+
+				break;
 		}
+
 		switch (font.params.v_align)
 		{
-		case wi::font::WIFALIGN_TOP:
-			font.params.posY = translation.y + 2 + margin_top;
-			break;
-		case wi::font::WIFALIGN_BOTTOM:
-			font.params.posY = translation.y + scale.y - 2 - margin_bottom;
-			break;
-		case wi::font::WIFALIGN_CENTER:
-		default:
-			font.params.posY = translation.y + scale.y * 0.5f;
-			break;
+			case wi::font::WIFALIGN_TOP:
+				font.params.posY = translation.y + 2 + margin_top;
+
+				break;
+			case wi::font::WIFALIGN_BOTTOM:
+				font.params.posY = translation.y + scale.y - 2 - margin_bottom;
+
+				break;
+			case wi::font::WIFALIGN_CENTER:
+			default:
+				font.params.posY = translation.y + scale.y * 0.5f;
+
+				break;
 		}
 
 		const float textheight = font.TextHeight();
 
 		if (fittext_enabled)
 		{
-			SetSize(XMFLOAT2(GetSize().x, textheight + 4 + margin_top + margin_bottom));
+			SetSize(XMFLOAT2(
+				GetSize().x,
+				textheight + 4 + margin_top + margin_bottom
+			));
 		}
 
 		if (scrollbar.IsEnabled())
@@ -109,16 +121,22 @@ namespace wi::gui
 			}
 
 			const Hitbox2D pointerHitbox = GetPointerHitbox();
-			if (scroll_allowed && scrollbar.IsEnabled() && scrollbar.IsScrollbarRequired() && pointerHitbox.intersects(hitBox))
-			{
+
+			if (scroll_allowed && scrollbar.IsEnabled()
+			  && scrollbar.IsScrollbarRequired()
+			  && pointerHitbox.intersects(hitBox)
+			) {
 				const float wheel_delta = wi::input::GetPointer().z;
 				const bool at_begin = (wheel_delta > 0 && scrollbar.IsScrolledToBegin());
+
 				if (wheel_delta != 0.0f && !at_begin)
 				{
 					scroll_allowed = false;
-					// This is outside scrollbar code, because it can also be scrolled if parent widget is only in focus
+					// This is outside scrollbar code, because it can also be
+					// scrolled if parent widget is only in focus.
 					scrollbar.Scroll(wheel_delta * 60.0f);
 				}
+
 				state = FOCUS;
 			}
 			else
@@ -126,8 +144,9 @@ namespace wi::gui
 				state = IDLE;
 			}
 
-			if (pointerHitbox.intersects(hitBox) && wi::input::Press(wi::input::MOUSE_BUTTON_LEFT))
-			{
+			if (pointerHitbox.intersects(hitBox)
+			  && wi::input::Press(wi::input::MOUSE_BUTTON_LEFT)
+			) {
 				Activate();
 			}
 		}
@@ -138,12 +157,15 @@ namespace wi::gui
 			{
 				font.params.h_wrap = scale.x - scrollbar_width;
 			}
+
 			font.params.posY += scrollbar.GetOffset();
 		}
 	}
+
 	void Label::Render(const wi::Canvas& canvas, const CommandList cmd) const
 	{
 		Widget::Render(canvas, cmd);
+
 		if (!IsVisible())
 		{
 			return;
@@ -158,7 +180,8 @@ namespace wi::gui
 			fx.pos.y -= shadow;
 			fx.siz.x += shadow * 2;
 			fx.siz.y += shadow * 2;
-			fx.color = shadow_color;
+			fx.color  = shadow_color;
+
 			if (fx.isCornerRoundingEnabled())
 			{
 				for (auto& corner_rounding : fx.corners_rounding)
@@ -169,17 +192,19 @@ namespace wi::gui
 					}
 				}
 			}
+
 			if (shadow_highlight)
 			{
 				fx.enableHighlight();
-				fx.highlight_pos = GetPointerHighlightPos(canvas);
-				fx.highlight_color = shadow_highlight_color;
+				fx.highlight_pos    = GetPointerHighlightPos(canvas);
+				fx.highlight_color  = shadow_highlight_color;
 				fx.highlight_spread = shadow_highlight_spread;
 			}
 			else
 			{
 				fx.disableHighlight();
 			}
+
 			wi::image::Draw(nullptr, fx, cmd);
 		}
 
@@ -193,11 +218,13 @@ namespace wi::gui
 			scrollbar.Render(canvas, cmd);
 		}
 	}
+
 	void Label::SetColor(const wi::Color color, const int id)
 	{
 		Widget::SetColor(color, id);
 		scrollbar.SetColor(color, id);
 	}
+
 	void Label::SetTheme(const Theme& theme, const int id)
 	{
 		Widget::SetTheme(theme, id);
