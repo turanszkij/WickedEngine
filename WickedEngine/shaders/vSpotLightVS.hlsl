9b15ee0027ffb649a60c550bdd5d066b2ae515ca
Turánszki János <turanszkij@gmail.com>
2026-07-03 07:07:41 +0200

    fix

--- WickedEngine/shaders/vSpotLightVS.hlsl
+++ WickedEngine/shaders/vSpotLightVS.hlsl
@@ -2,7 +2,7 @@
 #include "cone.hlsli"
 #include "lightingHF.hlsli"
 
-void main(uint vID : SV_VertexID, uint instanceID : SV_InstanceID, out float4 pos : SV_Position, out float4 col : COLOR, out uint rtindex : SV_RenderTargetArrayIndex)
+void main(uint vID : SV_VertexID, uint instanceID : SV_InstanceID, out float4 pos : SV_Position, out half4 col : COLOR, out uint rtindex : SV_RenderTargetArrayIndex)
 {
 	pos = CONE[vID];
 	col = float4(xLightColor.rgb, 1) * saturate(1 - dot(pos.xyz, pos.xyz));
